88d8d6783280302fccf5987045250f3fcb57c547
Brian Galey <bkgaley@gmail.com>
2022-02-28 16:50:33 -0800

    Change package name to climakitae

--- README.rst
+++ README.rst
@@ -1,2 +1,2 @@
-ClimaKit
-========
+climakitae
+==========
--- setup.py
+++ setup.py
@@ -5,13 +5,13 @@ from setuptools import setup, find_packages
 with open('README.rst') as fp:
     _description = fp.read()
 
-setup(name='climakit',
+setup(name='climakitae',
       version='0.0.1',
       description='Climate data analysis toolkit',
       long_description=_description,
       author='Cal-Adapt Analytics Engine Team',
       author_email='analytics@cal-adapt.org',
-      url='https://github.com/cal-adapt/climakit',
+      url='https://github.com/cal-adapt/climakitae',
       license='BSD',
       packages=find_packages(exclude=('tests', 'docs')),
       classifiers=['Development Status :: 1 - Planning'])
