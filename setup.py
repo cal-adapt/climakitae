3b7a07528b29b1d5686770bed5154d437c4e8a0e
Brian Galey <bkgaley@gmail.com>
2022-03-02 12:18:33 -0800

    Move package metadata to setup.cfg

--- setup.py
+++ setup.py
@@ -1,17 +1,4 @@
 #!/usr/bin/env python
+from setuptools import setup
 
-from setuptools import setup, find_packages
-
-with open('README.rst') as fp:
-    _description = fp.read()
-
-setup(name='climakitae',
-      version='0.0.1',
-      description='Climate data analysis toolkit',
-      long_description=_description,
-      author='Cal-Adapt Analytics Engine Team',
-      author_email='analytics@cal-adapt.org',
-      url='https://github.com/cal-adapt/climakitae',
-      license='BSD',
-      packages=find_packages(exclude=('tests', 'docs')),
-      classifiers=['Development Status :: 1 - Planning'])
+setup()
