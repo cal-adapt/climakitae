3513a96c99743b46ec3582e6dbc4e449d4162b1f
Brian Galey <bkgaley@gmail.com>
2022-03-04 13:37:22 -0800

    Add docs theme options

--- docs/conf.py
+++ docs/conf.py
@@ -36,6 +36,17 @@ exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
 # The theme to use for HTML and HTML Help pages. See the documentation for
 # a list of builtin themes.
 html_theme = 'sphinx_book_theme'
+# Theme options are theme-specific and customize the look and feel of a theme
+# further. For a list of options available for each theme, see the
+# documentation.
+html_theme_options = {
+    'repository_url': 'https://github.com/cal-adapt/climakitae',
+    'repository_branch': 'main',
+    'path_to_docs': 'docs',
+    'use_edit_page_button': True,
+    'use_repository_button': True,
+    'use_issues_button': True,
+}
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
