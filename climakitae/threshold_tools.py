e7fa20d6416b7eb6dd810bdbbe3106eb6d80daa6
Nabig Chaudhry <nabig@lbl.gov>
2022-03-28 13:31:06 -0700

    added back dask arguments

--- climakitae/threshold_tools.py
+++ climakitae/threshold_tools.py
@@ -189,7 +189,8 @@ def get_aicc_stat(ams, multiple_points=True):
         input_core_dims=[["time"]],
         exclude_dims=set(("time",)),
         output_core_dims=[[], [], []],
-        dask = "parallelized"
+        dask = "parallelized",
+        dask_gufunc_kwargs=dict("allow_rechunk"=True, "output_dtypes"=[ams.dtype])
     )
 
     all_aicc_results = all_aicc_results.rename("all_aicc_results")
