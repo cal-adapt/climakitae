f21f4626bc46c7a01a77d8992402b102479a79e7
Cora Kingdon <ckingdon@berkeley.edu>
2022-07-11 09:29:10 -0500

    use pytest.fixture for tes data

--- tests/test_timeseriestools.py
+++ tests/test_timeseriestools.py
@@ -1,4 +1,3 @@
-import climakitae as ck
 from climakitae import timeseriestools as tst
 import datetime as dt
 import pytest
@@ -6,14 +5,16 @@ import xarray as xr
 
 #------------- Read in the test dataset ---------------------------------------
 
-# This data is generated in "create_timeseries_test_data.py"
-test_filepath = "tests/test_data/timeseries_data_T2_2014_2016_monthly_45km.nc"
-y = xr.open_dataarray(test_filepath)
+@pytest.fixture
+def test_data():
+    # This data is generated in "create_timeseries_test_data.py"
+    test_filepath = "tests/test_data/timeseries_data_T2_2014_2016_monthly_45km.nc"
+    return xr.open_dataarray(test_filepath)
 
 #------------- Test monthly weighted running mean without anomaly -------------
 
-def test_monthly_smoothing():
-    ts = tst.Timeseries(y) 
+def test_monthly_smoothing(test_data):
+    ts = tst.Timeseries(test_data) 
     tsp = ts.choices
     tsp.smoothing = "running mean"
     tsp.num_timesteps = 3
@@ -24,11 +25,11 @@ def test_monthly_smoothing():
 
 #------------- Test monthly weighted anomaly -----------------------------------
 
-def test_monthly_anomaly():
-    ts = tst.Timeseries(y) 
+def test_monthly_anomaly(test_data):
+    ts = tst.Timeseries(test_data) 
     tsp = ts.choices
     tsp.anomaly = True
     tsp.reference_range = (dt.datetime(2014, 1, 1), dt.datetime(2014, 12, 31)) 
 
     tr_data = tsp.transform_data()
-    assert (tr_data == y).sum().values.item() == 0
+    assert (tr_data == test_data).sum().values.item() == 0
