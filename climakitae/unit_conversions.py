e273af7322146c110a311f8222d6038990cdca1d
Victoria Ford <fordvecellio@gmail.com>
2022-07-27 10:34:00 -0400

    fixing typos + degF conversion + inHg conversion

--- climakitae/unit_conversions.py
+++ climakitae/unit_conversions.py
@@ -1,44 +1,44 @@
 """
-This script calculates alternative units for variables with multiplecommonly used units.
+This script calculates alternative units for variables with multiple commonly used units.
 
 """
 
-def _convert_units(da, native_units, selected_units): 
+def _convert_units(da, native_units, selected_units):
     """ Converts units for any variable
 
     Args:
       da (xr.DataArray): Data
-      native_units (str): Native units of data, from selections.native_units 
+      native_units (str): Native units of data, from selections.native_units
       selected_units (str): Selected units of data, from selections.units
 
     Returns:
       da (xr.DataArray): Data with converted units and updated units attribute
     """
-    
+
     # Units that have existing conversions
     units_with_conversions = ["mm","Pa","m/s","K","kg/kg"]
-    
-    # Pass if chosen units is the same as native units 
-    if native_units == selected_units: 
+
+    # Pass if chosen units is the same as native units
+    if native_units == selected_units:
         pass
-    
+
     # Raise error; Unit selected exists as button, but is not completely integrated into the code selection process.
     elif (native_units != selected_units) and (native_units not in units_with_conversions):
         raise ValueError("You've encountered a bug in the code. Selected unit " + selected_units + " is not a valid unit option. Check package data and/or source code for data_loaders and unit_conversions modules.")
-    
-    # Precipitation units 
+
+    # Precipitation units
     elif native_units == "mm":
         if selected_units == "inches":
             da = da / 25.4
             da.attrs["units"] = selected_units
-            
-    # Weight ratio units
+
+    # Moisture ratio units
     elif native_units == "kg/kg":
         if selected_units == "g/kg":
             da = da * 1000
             da.attrs["units"] = selected_units
 
-    # Pressure units 
+    # Pressure units
     elif native_units == "Pa":
         if selected_units == "hPa":
             da = da / 100.
@@ -47,24 +47,24 @@ def _convert_units(da, native_units, selected_units):
             da = da / 100.
             da.attrs["units"] = selected_units
         elif selected_units == "inHg":
-            da = da / 100. / 29.92 
+            da = da / 3386.39
             da.attrs["units"] = selected_units
 
-    # Wind units 
+    # Wind units
     elif native_units == "m/s":
         if selected_units == "knots":
             da = da * 1.94
             da.attrs["units"] = selected_units
 
     # Temperature units
-    elif native_units == "K": 
+    elif native_units == "K":
         if selected_units == "degC":
             da = da - 273.15
             da.attrs["units"] = selected_units
         elif selected_units == "degF":
-            da = 1.8 * (da - 273.15) - 32
+            da = 1.8 * (da - 273.15) + 32
             da.attrs["units"] = selected_units
-        elif selected_units == "degR": 
+        elif selected_units == "degR":
             da = da * 1.8
             da.attrs["units"] = selected_units
 
