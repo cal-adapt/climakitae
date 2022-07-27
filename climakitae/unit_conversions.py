c2b9adea3df8df970cbbd49d0ae4d84fb96ce394
Nicole Keeney <nicolejkeeney@gmail.com>
2022-07-27 11:50:26 -0700

    Add test to confirm that unit conversion exists for all options in csv file. Remove Error from module to catch this error, because the test should do that for us

--- climakitae/unit_conversions.py
+++ climakitae/unit_conversions.py
@@ -15,16 +15,9 @@ def _convert_units(da, native_units, selected_units):
       da (xr.DataArray): Data with converted units and updated units attribute
     """
 
-    # Units that have existing conversions
-    units_with_conversions = ["mm","Pa","m/s","K","kg/kg"]
-
     # Pass if chosen units is the same as native units
     if native_units == selected_units:
         pass
-
-    # Raise error; Unit selected exists as button, but is not completely integrated into the code selection process.
-    elif (native_units != selected_units) and (native_units not in units_with_conversions):
-        raise ValueError("You've encountered a bug in the code. Selected unit " + selected_units + " is not a valid unit option. Check package data and/or source code for data_loaders and unit_conversions modules.")
 
     # Precipitation units
     elif native_units == "mm":
