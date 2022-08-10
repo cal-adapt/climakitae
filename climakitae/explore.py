899f0c35b93f02606516a25d65a377da6f3b0d32
Nicole Keeney <nicolejkeeney@gmail.com>
2022-08-10 08:26:52 -0700

    remove progress bar, causes issues with plotting

--- climakitae/explore.py
+++ climakitae/explore.py
@@ -1,10 +1,10 @@
-from functools import wraps
-from dask import delayed, compute
 import hvplot.xarray
 import cartopy.crs as ccrs
-from dask.diagnostics import ProgressBar
-pbar = ProgressBar()
-pbar.register()
+#from functools import wraps
+#from dask import delayed, compute
+#from dask.diagnostics import ProgressBar
+#pbar = ProgressBar()
+#pbar.register()
 
 def _explore(data_to_use):
     da_plt = data_to_use.hvplot.quadmesh(
@@ -15,25 +15,25 @@ def _explore(data_to_use):
     
     return da_plt
     
-def progress_bar(func):
-    @wraps(func)
-    def pbar_wrapper(*args, **kwargs):
-        """
-        Generic decorator that shows a progress bar for any
-        function using dask delayed and compute functionality.
-        To use, put the following two lines above the 
-        function definition (in this order):
-        @progress_bar
-        @delayed        
-        """          
-        with pbar:
-            
-            print(f"Request in progress. This may take a while. "+
-                 "Thanks for your patience!")
-
-            the_request = func(*args, **kwargs).compute()
-            
-        return(the_request)
-        
-    return(pbar_wrapper)
+#def progress_bar(func):
+#    @wraps(func)
+#    def pbar_wrapper(*args, **kwargs):
+#        """
+#        Generic decorator that shows a progress bar for any
+#        function using dask delayed and compute functionality.
+#        To use, put the following two lines above the 
+#        function definition (in this order):
+#        @progress_bar
+#        @delayed        
+#        """          
+#        with pbar:
+#            
+#            print(f"Request in progress. This may take a while. "+
+#                 "Thanks for your patience!")
+#
+#            the_request = func(*args, **kwargs).compute()
+#            
+#        return(the_request)
+#        
+#    return(pbar_wrapper)
 
