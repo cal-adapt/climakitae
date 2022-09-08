8b865cf8f7c08017fa32364e1e933427a91ed175
Nicole Keeney <nicolejkeeney@gmail.com>
2022-09-08 11:43:52 -0500

    base code for app.view() added

--- climakitae/view.py
+++ climakitae/view.py
@@ -1,14 +1,31 @@
 import xarray as xr 
 import hvplot.xarray
-import holoviews as hv
-from holoviews import opts
 
 def _visualize(data): 
-    """Make single map"""
-    data_i = data.isel(time=0,scenario=0,simulation=0)
-    _plot = data_i.hvplot.image(
-        x="x", y="y", 
-        grid=True, 
-        title="Generic map"
-    )
+    """Create a generic plot or map of the data"""
+    
+    # Workflow if data contains spatial coordinates 
+    if set(["x","y"]).issubset(set(data.dims)):
+
+        # Plotting inputs
+        non_spatial_dims = [dim for dim in data.dims if dim not in ["x","y"]]
+        try: 
+            clabel = data.name + " ("+data.attrs["units"]+")"
+        except: # Try except just in case units attribute is missing from data 
+            clabel = data.name
+
+        # Create map 
+        _plot = data.hvplot.image(
+            x="x", y="y", 
+            grid=True, 
+            groupby=non_spatial_dims, 
+            clabel=clabel
+        )
+
+    # Workflow if data contains only time dimension
+    elif "time" in data.dims: 
+
+        # Create lineplot
+        _plot = data.hvplot.line(x="time")
+        
     return _plot
