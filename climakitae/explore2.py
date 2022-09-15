06dbbda14de68440c2d3309f0853fe5d0e2c22bb
Cora Kingdon <ckingdon@berkeley.edu>
2022-09-15 11:04:41 -0700

    start ThresholdDataParams class

--- climakitae/explore2.py
+++ climakitae/explore2.py
@@ -2,7 +2,7 @@
 
 import panel as pn
 
-from .threshold_tools import ExceedanceParams, _exceedance_visualize
+from .threshold_tools import ThresholdDataParams, ExceedanceParams, _exceedance_visualize
 
 class AppExplore(object):
     """
@@ -12,13 +12,43 @@ class AppExplore(object):
     """
 
     def __init__(self, selections, location, _cat):
-        self.selections = selections
-        self.location = location,
-        self._cat = _cat
+        self.selections2 = selections
+        self.location2 = location,
+        self._cat2 = _cat
 
     def TMY(self):
         return pn.Card(title = "Typical Meteorological Year", collapsible = False)
 
     def thresholds(self, da, option=1):
+        thresh_data = ThresholdDataParams(selections=self.selections2, location=self.location2)
+
+        data_options_card = pn.Card(
+            pn.Row(
+                pn.Column(
+                    pn.widgets.Select.from_param(thresh_data.param.variable2, name="Data variable"),
+                    pn.widgets.StaticText.from_param(selections.param.variable_description),
+                    width = 230),
+                pn.Column(
+                    pn.widgets.Select.from_param(thresh_data.param.area_subset2, name="Area subset"),
+                    pn.widgets.Select.from_param(thresh_data.param.cached_area2, name="Cached area"),
+                    location.view,
+                    width = 230)
+                )
+        , title="Data Options", collapsible=False, width=460, height=515
+        )
+
+        description_box = pn.Card(
+            "Description text",
+            title = "About this tool", collapsible = False
+        )
+
         exc_choices = ExceedanceParams(da) # initialize an instance of the Param class for this dataarray
-        return _exceedance_visualize(exc_choices, option) # display the holoviz panel
+
+        plot_panel = _exceedance_visualize(exc_choices, option) # display the holoviz panel
+
+        return pn.Column(
+            pn.Row(
+                data_options_card, description_box
+            ),
+            plot_panel
+        )
--- climakitae/threshold_tools.py
+++ climakitae/threshold_tools.py
@@ -1146,6 +1146,65 @@ def _exceedance_plot_subtitle(exceedance_count):
 
 #------------- Class and methods for the explore_exceedance GUI ---------------
 
+class ThresholdDataParams(param.Parameterized):
+    """
+    An object that holds the "Data Options" parameters for the 
+    explore.thresholds panel. 
+    """
+    def __init__(self, *args, **params):
+        super().__init__(*args, **params)
+        
+        # Selectors defaults
+        self.selections.append_historical = False
+        self.selections.area_average = True
+        self.selections.resolution = "45 km"
+        self.selections.scenario = ["SSP 3-7.0 -- Business as Usual"]
+        self.selections.time_slice = (1980,2100)
+        self.selections.timescale = "hourly"
+        self.selections.variable = "Air Temperature at 2m"
+
+        # Location defaults
+        self.location.area_subset = 'states'
+        self.location.cached_area = 'CA'
+
+    variable2 = param.ObjectSelector(default="Air Temperature at 2m",
+        objects=["Air Temperature at 2m"]
+    )
+
+    cached_area2 = param.ObjectSelector(default="CA",
+        objects=["CA"]
+    )
+
+    area_subset2 = param.ObjectSelector(
+        default="states",
+        objects=["states", "CA counties"],
+    )
+
+    @param.depends("variable2", watch=True)
+    def _update_variable(self):
+        """Update variable in selections object to reflect variable chosen in panel"""
+        self.selections.variable = self.variable2
+
+    @param.depends("area_subset2", watch=True)
+    def _update_cached_area(self):
+        """
+        Makes the dropdown options for 'cached area' reflect the type of area subsetting
+        selected in 'area_subset' (currently state, county, or watershed boundaries).
+        """
+        if self.area_subset2 == "CA counties":
+            # setting this to the dict works for initializing, but not updating an objects list:
+            self.param["cached_area2"].objects = ["Santa Clara County", "Los Angeles County"]
+            self.cached_area2 = "Santa Clara County"
+        elif self.area_subset2 == "states":
+            self.param["cached_area2"].objects = ["CA"]
+            self.cached_area2 = "CA"
+
+    @param.depends("area_subset2","cached_area2",watch=True)
+    def _updated_location(self):
+        """Update locations object to reflect location chosen in panel"""
+        self.location.area_subset = self.area_subset2
+        self.location.cached_area = self.cached_area2
+
 class ExceedanceParams(param.Parameterized):
     """
     An object to hold exceedance count parameters, which depends on the 'param' library.
