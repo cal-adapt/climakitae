e2ce2b1fa69905965d134ea363fe67390206668a
Victoria Ford <fordvecellio@gmail.com>
2022-09-27 16:46:38 -0400

    panel updates & ae colormaps

--- climakitae/tmy.py
+++ climakitae/tmy.py
@@ -32,12 +32,15 @@ import panel as pn
 import intake
 import warnings
 from .data_loaders import _read_from_catalog
-# from .utils import _read_var_csv, _read_ae_colormap
+from .utils import _read_ae_colormap, _read_var_csv
 import intake
 import pkg_resources
 
 import logging  # Silence warnings
 logging.getLogger("param").setLevel(logging.CRITICAL)
+
+var_descrip_pkg = pkg_resources.resource_filename('climakitae', 'data/variable_descriptions.csv')
+var_descrip = _read_var_csv(var_descrip_pkg, index_col="description")
 
 xr.set_options(keep_attrs=True) # Keep attributes when mutating xr objects
 
@@ -98,6 +101,43 @@ class AverageMeteorologicalYear(param.Parameterized):
     cached_area2 = param.ObjectSelector(default="CA", objects=dict())
     area_subset2 = param.ObjectSelector(default="states", objects=["CA counties", "states"])
 
+    # Create dictionary of TMY advanced options depending on TMY type
+    tmy_advanced_options_dict = {
+        "Absolute": {
+            "default":"Historical",
+            "objects":["Historical","Warming Level Future"]
+        },
+        "Difference": {
+            "default":"Warming Level Future",
+            "objects":["Warming Level Future"] #,"Severe AMY"]
+        }
+    }
+
+    # Create a dictionary that briefly explains the computation being perfomed
+    computatation_description_dict = {
+        "Absolute": {
+            "Historical": "AMY computed using the historical baseline for 1981-2010.",
+            "Warming Level Future": "AMY computed using the 30-year future period centered around when the selected warming level is reached."
+        },
+        "Difference": {
+            "Warming Level Future": "AMY computed by taking the difference between the 30-year future period centered around the selected warming level and the historical baseline."
+            # "Severe AMY": "AMY computed by taking the difference between the 90th percentile of the 30-year future period centered around the selected warming level and the historical baseline."
+        }
+    }
+
+    # Define TMY params
+    tmy_options = param.ObjectSelector(
+        default='Absolute',
+        objects=['Absolute', 'Difference']
+    )
+
+    # Define new advanced options param, that is dependent on the user selection in tmy_options
+    tmy_advanced_options = param.ObjectSelector(objects=dict())
+
+    # Define new computation description param
+    # This will provide a string description of the computation option selected and will update dynamically depending on the user selections
+    tmy_computation_description = param.ObjectSelector(objects=dict())
+
     def __init__(self, *args, **params):
         super().__init__(*args, **params)
 
@@ -119,6 +159,13 @@ class AverageMeteorologicalYear(param.Parameterized):
         self.param["variable2"].objects = self.selections.param.variable.objects
         self.param["cached_area2"].objects = self.location.param.cached_area.objects
 
+        # Initialze tmy_adanced_options param
+        self.param["tmy_advanced_options"].objects = self.tmy_advanced_options_dict[self.tmy_options]["objects"]
+        self.tmy_advanced_options = self.tmy_advanced_options_dict[self.tmy_options]["default"]
+
+        # Initialize tmy_computation_description param
+        self.tmy_computation_description = self.computatation_description_dict[self.tmy_options][self.tmy_advanced_options]
+
         # Postage data and anomalies defaults
         self.historical_tmy_data = _get_historical_tmy_data(
             selections=self.selections,
@@ -131,16 +178,6 @@ class AverageMeteorologicalYear(param.Parameterized):
             catalog=self.catalog,
             warmlevel=1.5
         )
-
-    # TMY options to display
-    tmy_options = param.ObjectSelector(default='Absolute',
-        objects=['Absolute', 'Difference'])
-
-    abs_tmy_options = param.ObjectSelector(default='Historical',
-        objects=['Historical', 'Warming Level Future'])
-
-    diff_tmy_options = param.ObjectSelector(default='Warming Level Future',
-        objects=['Warming Level Future', 'Severe AMY'])
 
     warmlevel = param.ObjectSelector(default=1.5,
         objects=[1.5, 2, 3])
@@ -164,13 +201,11 @@ class AverageMeteorologicalYear(param.Parameterized):
         selected in 'area_subset' (currently state, county, or watershed boundaries).
         """
         if self.area_subset2 == "CA counties":
-            # Get full list of counties
             self.param["cached_area2"].objects = list(
                 self.location._geography_choose[self.area_subset2].keys()
             )
             self.cached_area2 = "Sacramento County"
         elif self.area_subset2 == "states":
-            # Only CA for now?
             self.param["cached_area2"].objects = ["CA"]
             self.cached_area2 = "CA"
 
@@ -196,10 +231,18 @@ class AverageMeteorologicalYear(param.Parameterized):
             warmlevel=self.warmlevel
         )
 
+    # Create a function that will update tmy_advanced_options when tmy_options is modified
+    @param.depends("tmy_options", watch=True)
+    def _update_tmy_advanced_options(self):
+        self.param["tmy_advanced_options"].objects = self.tmy_advanced_options_dict[self.tmy_options]["objects"]
+        self.tmy_advanced_options = self.tmy_advanced_options_dict[self.tmy_options]["default"]
+
+    @param.depends("tmy_options", "tmy_advanced_options", watch=True)
+    def _update_tmy_computatation_description(self):
+        self.tmy_computation_description = self.computatation_description_dict[self.tmy_options][self.tmy_advanced_options]
 
     @param.depends("reload_data", watch=False)
     def _tmy_hourly_heatmap(self):
-
         def remove_repeats(xr_data):
             """
             Remove hours that have repeats.
@@ -252,22 +295,23 @@ class AverageMeteorologicalYear(param.Parameterized):
 
         # update heatmap df and title with selections
         if self.tmy_options == "Absolute":
-            if self.abs_tmy_options == "Historical":
+            if self.tmy_advanced_options == "Historical":
                 df = df_hist
-                title = "Average Meteorological Year\nAbsolute {} Baseline \n{}".format(self.abs_tmy_options, self.cached_area2)
+                title = "Average Meteorological Year: {}\nAbsolute {} Baseline".format(self.cached_area2, self.tmy_advanced_options)
                 clabel = self.variable2 + " (" +self.historical_tmy_data.attrs["units"]+")"
             else:
                 df = df_future
-                title = "Average Meteorological Year\nAbsolute {} at {}°C \n{}".format(self.abs_tmy_options, self.warmlevel, self.cached_area2)
+                title = "Average Meteorological Year: {}\nAbsolute {} at {}°C".format(self.cached_area2, self.tmy_advanced_options, self.warmlevel)
                 clabel = self.variable2 + " (" +self.future_tmy_data.attrs["units"]+")"
         elif self.tmy_options == "Difference":
-            if self.diff_tmy_options == "Warming Level Future":
+            cmap = _read_ae_colormap("ae_diverging", cmap_hex = True)
+            if self.tmy_advanced_options == "Warming Level Future":
                 df = df_future - df_hist
-                title = "Average Meteorological Year\nDifference between {} at {}°C and Historical Baseline \n{}".format(self.diff_tmy_options, self.warmlevel, self.cached_area2)
+                title = "Average Meteorological Year: {}\nDifference between {} at {}°C and Historical Baseline".format(self.cached_area2, self.tmy_advanced_options, self.warmlevel)
                 clabel = self.variable2 + " (" +self.historical_tmy_data.attrs["units"]+")"
             else:
                 df = df_future - df_hist # placeholder for now for severe amy
-                title = "Average Meteorological Year\nDifference between {} at 90th percentile and Historical Baseline \n{}".format(self.diff_tmy_options, self.cached_area2)
+                title = "Average Meteorological Year: {}\nDifference between {} at 90th percentile and Historical Baseline".format(self.cached_area2, self.tmy_advanced_options)
                 clabel = self.variable2 + " (" +self.historical_tmy_data.attrs["units"]+")"
         else:
             title = "Average Meteorological Year\n{}".format(self.cached_area2)
@@ -276,17 +320,23 @@ class AverageMeteorologicalYear(param.Parameterized):
         df = df[[8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,1,2,3,4,5,6,7]]
         df.columns = ['12am','1am','2am','3am','4am','5am','6am','7am','8am','9am','10am','11am','12pm','1pm','2pm','3pm','4pm','5pm','6pm','7pm','8pm','9pm','10pm','11pm']
 
+        cmap_name = var_descrip[self.variable2]["default_cmap"]
+        cmap = _read_ae_colormap(cmap=cmap_name, cmap_hex=True)
+        # if df.min() < 0:
+        #     cmap = _read_ae_colormap(cmap="ae_diverging", cmap_hex = True)
+
         heatmap = df.hvplot.heatmap(
             x='columns',
             y='index',
             title=title,
-            cmap="YlOrRd",
+            cmap=cmap,
             xaxis='bottom',
             xlabel="Hour of Day (PST)",
             ylabel="Day of Year", clabel=clabel, rot=60,
             width=800, height=350).opts(
             fontsize={'title': 15, 'xlabel':12, 'ylabel':12}
         )
+        heatmap.opts(toolbar="below")
 
         return heatmap
 
@@ -298,9 +348,10 @@ def _amy_visualize(tmy_ob, selections, location):
     user_options = pn.Card(
             pn.Row(
                 pn.Column(
-                    pn.widgets.RadioButtonGroup.from_param(tmy_ob.param.tmy_options, name=" "),
-                    pn.widgets.Select.from_param(tmy_ob.param.abs_tmy_options, name="Absolute AMY Options"),
-                    pn.widgets.Select.from_param(tmy_ob.param.diff_tmy_options, name="Difference AMY Options"),
+                    pn.widgets.StaticText(name="", value="Average Meteorological Year Type"),
+                    pn.widgets.RadioButtonGroup.from_param(tmy_ob.param.tmy_options),
+                    pn.widgets.Select.from_param(tmy_ob.param.tmy_advanced_options, name="Computation Options"),
+                    pn.widgets.StaticText.from_param(tmy_ob.param.tmy_computation_description, name=""),
                     pn.widgets.StaticText(name="", value="Warming level (°C)"),
                     pn.widgets.RadioButtonGroup.from_param(tmy_ob.param.warmlevel),
                     pn.widgets.Select.from_param(tmy_ob.param.variable2, name="Data variable"),
@@ -318,7 +369,9 @@ def _amy_visualize(tmy_ob, selections, location):
 
     mthd_bx = pn.Column(
         pn.widgets.StaticText(
-            value="An average meteorological year is calculated by selecting the 24 hours for every day that best represent multi-model mean conditions during a 30-year period – 1981-2010 for the historical baseline or centered on the year the warming level is reached. Absolute average meteorolgoical year profiles represent data that is not bias corrected, please exercise caution when analyzing. Warming level future aboslute data is based on 30 year period centered around when multi-model mean exceeds a selected warming level (please see explore_warming notebook for further details). The 'severe' AMY is calculated using the 90th percentile of future warming level data at the selected warming level, and is compared to the historical baseline.",
+            value="An average meteorological year is calculated by selecting the 24 hours for every day that best represent multi-model mean conditions during a 30-year period – 1981-2010 \
+            for the historical baseline or centered on the year the warming level is reached. Absolute average meteorolgoical year profiles represent data that is not bias corrected,  \
+            please exercise caution when analyzing. The 'severe' AMY is calculated using the 90th percentile of future warming level data at the selected warming level, and is compared to the historical baseline.",
             width=400
         ),
     )
@@ -326,7 +379,7 @@ def _amy_visualize(tmy_ob, selections, location):
     TMY = pn.Card(
         pn.widgets.StaticText(
            value="Absolute AMY",
-           width = 650, height=500
+           width = 600, height=500
            ),
         tmy_ob._tmy_hourly_heatmap
     )
