b85b8fa36450fce84800767d5114943fbd5e9bcd
Victoria Ford <fordvecellio@gmail.com>
2023-05-17 18:00:53 -0400

    add wind direction

--- climakitae/derive_variables.py
+++ climakitae/derive_variables.py
@@ -226,3 +226,25 @@ def _compute_wind_mag(u10, v10, name="wind_speed_derived"):
     wind_mag = np.sqrt(np.square(u10) + np.square(v10))
     wind_mag.name = "wind_speed_derived"
     return wind_mag
+
+
+def _compute_wind_dir(u10, v10, name="wind_direction_derived"):
+    """Compute wind direction at 10 meters
+
+    Args:
+        u10 (xr.DataArray): Zonal velocity at 10 meters height in m/s
+        v10 (xr.DataArray): Meridional velocity at 10 meters height in m/s
+        name (str, optional): Name to assign to output DataArray
+
+    Returns:
+        wind_dir (xr.DataArray): Wind direction, in [0, 360] degrees,
+            with 0/360 defined as north, by meteorological convention
+
+    Notes:
+    1. https://sites.google.com/view/raybellwaves/cheat-sheets/xarray
+    2. must be calculated as `xr.apply_ufunc(_compute_wind_dir, u10, v10)`
+    """
+
+    wind_dir = np.mod(90 - np.arctan2(-v10, -u10) * (180 / np.pi), 360)
+    return wind_dir
+
