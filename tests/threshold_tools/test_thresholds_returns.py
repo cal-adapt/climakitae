d54a16b2eaa4545458b3e4224c541ad4239568a1
Cora Kingdon <ckingdon@berkeley.edu>
2023-06-13 12:44:42 -0700

    update thresholds unit tests for get_block_maxima

--- tests/threshold_tools/test_thresholds_returns.py
+++ tests/threshold_tools/test_thresholds_returns.py
@@ -21,7 +21,7 @@ def T2_ams(rootdir):
     test_filename = "test_data/timeseries_data_T2_2014_2016_monthly_45km.nc"
     test_filepath = os.path.join(rootdir, test_filename)
     test_data = xr.open_dataset(test_filepath).T2
-    return threshold_tools.get_ams(test_data).isel(simulation=0)
+    return threshold_tools.get_block_maxima(test_data).isel(simulation=0)
 
 
 # ------------- Test return values and periods ----------------------------------
@@ -65,21 +65,21 @@ def test_return_period_invalid_distr(T2_ams):
 # Test that the AMS (block maxima) for a 3-day grouped event are lower than 
 # the simple AMS (single hottest value in each year)
 def test_ams_ex1(T2_hourly):
-    ams = threshold_tools.get_ams(T2_hourly)
-    ams_3d = threshold_tools.get_ams(T2_hourly, groupby=(1, 'day'), grouped_duration=(3, 'day'))
+    ams = threshold_tools.get_block_maxima(T2_hourly)
+    ams_3d = threshold_tools.get_block_maxima(T2_hourly, groupby=(1, 'day'), grouped_duration=(3, 'day'))
     assert (ams > ams_3d).all()
 
 
 # Test that the AMS (block maxima) for a 3-day continous event are lower than 
 # the AMS for a grouped 3-day event
 def test_ams_ex2(T2_hourly):
-    ams_3d = threshold_tools.get_ams(T2_hourly, groupby=(1, 'day'), grouped_duration=(3, 'day'))
-    ams_72h = threshold_tools.get_ams(T2_hourly, duration=(72, 'hour'))
+    ams_3d = threshold_tools.get_block_maxima(T2_hourly, groupby=(1, 'day'), grouped_duration=(3, 'day'))
+    ams_72h = threshold_tools.get_block_maxima(T2_hourly, duration=(72, 'hour'))
     assert (ams_3d > ams_72h).all()
 
 # Test that the AMS (block maxima) for a 4-hour per day for 3 days are lower 
 # than the AMS for a grouped 3-day event
 def test_ams_ex3(T2_hourly):
-    ams_3d = threshold_tools.get_ams(T2_hourly, groupby=(1, 'day'), grouped_duration=(3, 'day'))
-    ams_3d_4h = threshold_tools.get_ams(T2_hourly, duration=(4, 'hour'), groupby=(1, 'day'), grouped_duration=(3, 'day'))
+    ams_3d = threshold_tools.get_block_maxima(T2_hourly, groupby=(1, 'day'), grouped_duration=(3, 'day'))
+    ams_3d_4h = threshold_tools.get_block_maxima(T2_hourly, duration=(4, 'hour'), groupby=(1, 'day'), grouped_duration=(3, 'day'))
     assert (ams_3d > ams_3d_4h).all()
