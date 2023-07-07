bbe5fa80bdb655e87f64beb4e96d2478fbae786e
Nicole Keeney <nicolejkeeney@gmail.com>
2023-07-07 14:13:05 -0600

    noaa heat index

--- climakitae/indices.py
+++ climakitae/indices.py
@@ -1,6 +1,73 @@
-"""Functions for deriving fire indices"""
+"""Functions for deriving indices"""
 
 import xarray as xr
+import numpy as np
+
+
+def noaa_heat_index(T, RH):
+    """Compute the NOAA Heat Index
+
+    Parameters
+    ----------
+    T: xr.DataArray
+        Temperature in deg F
+    RH: xr.DataArray
+        Relative Humidity in percentage (0-100)
+
+    Returns
+    --------
+    xr.DataArray
+        Heat index per timestep
+
+    References
+    -----------
+    https://www.wpc.ncep.noaa.gov/html/heatindex_equation.shtml
+
+    """
+    T = T.reindex_like(RH)  # Need to have the same dimension/coordinate orders
+    HI = (
+        -42.379
+        + 2.04901523 * T
+        + 10.14333127 * RH
+        - 0.22475541 * T * RH
+        - 0.00683783 * T * T
+        - 0.05481717 * RH * RH
+        + 0.00122874 * T * T * RH
+        + 0.00085282 * T * RH * RH
+        - 0.00000199 * T * T * RH * RH
+    )
+
+    # Adjust for high temperature, low relative humidity
+    # 80 < T < 112 (deg F)
+    # RH < 13%
+    adj_highT_lowRH = ((13 - RH) / 4) * ((17 - abs(T - 95)) / 17) ** (
+        1 / 2
+    )  # Adjustment
+    HI_highT_lowRH = HI - adj_highT_lowRH  # Subtract adjustment from HI
+
+    # Adjust for low temperature, high relative humidity
+    # 80 < T < 87 (deg F)
+    # RH > 85%
+    adj_lowT_highRH = ((RH - 85) / 10) * ((87 - T) / 5)  # Adjustment
+    HI_lowT_highRH = HI + adj_lowT_highRH  # Add adjustment from HI
+
+    # Use different equation if heat index if the heat index value < 80
+    low_HI = 0.5 * (T + 61.0 + ((T - 68.0) * 1.2) + (RH * 0.094))
+
+    # Adjust heat index depending on different condions for RH, T, and valid range of HI
+    HI = xr.where((RH < 13) & (T > 80), HI_highT_lowRH, HI)
+    HI = xr.where(((RH > 85) & (T < 87) & (T > 80)), HI_lowT_highRH, HI)
+    HI = xr.where(HI < 80, low_HI, HI)
+    HI = xr.where(
+        T > 112, np.nan, HI
+    )  # If temp is above 112 deg F, HI is not valid. Set to NaN
+
+    # Assign units attribute
+    HI.attrs["units"] = "degF"
+    return HI
+
+
+## ========== FOSBERG FIRE INDEX AND RELATED HELPER FUNCTIONS ==========
 
 
 def fosberg_fire_index(t2_F, rh_percent, windspeed_mph):
@@ -32,7 +99,7 @@ def fosberg_fire_index(t2_F, rh_percent, windspeed_mph):
     # Compute the equilibrium moisture constant
     m_low, m_mid, m_high = _equilibirum_moisture_constant(h=rh_percent, T=t2_F)
 
-    # For RH > 10%, use the low m value.
+    # For RH < 10%, use the low m value.
     # For RH >= 10%, use the mid value
     m = xr.where(rh_percent < 10, m_low, m_mid)
     # For RH > 50%, use the high m value.
@@ -46,7 +113,6 @@ def fosberg_fire_index(t2_F, rh_percent, windspeed_mph):
     FFWI = (n * ((1 + U**2) ** 0.5)) / 0.3002
 
     # Add descriptive attributes
-    FFWI.name = "Fosberg Fire Weather Index"
     FFWI.attrs["units"] = "[0-100]"
 
     return FFWI
