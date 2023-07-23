724442065ca60fdb7dbbe4585f29a669f9826876
Eric Lehmer <elehmer@berkeley.edu>
2023-07-23 14:17:03 -0700

    1st Refactor: move core classes to core dir

---
+++ climakitae/core/constants.py
@@ -0,0 +1,10 @@
+# constants.py
+"""This module defines package level constants"""
+
+# Local Data Paths
+variable_description_csv_path = "data/variable_descriptions.csv"
+stations_csv_path = "data/hadisd_stations.csv"
+
+# Intake Data Calalog URLs
+data_catalog_url = "https://cadcat.s3.amazonaws.com/cae-collection.json"
+boundary_catalog_url = "https://cadcat.s3.amazonaws.com/parquet/catalog.yaml"
---
+++ climakitae/core/data_interface.py
@@ -0,0 +1,20 @@
+import pkg_resources
+import pandas as pd
+import geopandas as gpd
+import intake
+from constants import variable_descriptions_csv_path, stations_csv_path, data_catalog_url
+
+
+class DataInterface:
+    def __init__(self):
+        self.variable_descriptions = pd.read_csv(variable_descriptions_csv_path, index_col=None)
+        self.stations = pd.read_csv(stations_csv_path)
+        self.data_catalog = intake.open_esm_datastore(data_catalog_url)
+        
+    def get_stations_gdf(self):
+        stations_gpf = gpd.GeoDataFrame(
+            self.stations,
+            crs="EPSG:4326",
+            geometry=gpd.points_from_xy(self.stations.LON_X, self.stations.LAT_Y),
+        )
+        return stations_gpf
