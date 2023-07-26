3006034c833e5e1de2ed400b693d6e2ace7d986e
Nicole Keeney <nicolejkeeney@gmail.com>
2023-07-26 16:54:48 -0600

    limit index to 0-100

--- climakitae/indices.py
+++ climakitae/indices.py
@@ -45,6 +45,12 @@ def fosberg_fire_index(t2_F, rh_percent, windspeed_mph):
     U = windspeed_mph
     FFWI = (n * ((1 + U**2) ** 0.5)) / 0.3002
 
+    # If fosberg index > 100, reset to 100
+    FFWI = xr.where(FFWI < 100, FFWI, 100, keep_attrs=True)
+
+    # If fosberg index is negative, set to 0
+    FFWI = xr.where(FFWI > 0, FFWI, 0, keep_attrs=True)
+
     # Add descriptive attributes
     FFWI.name = "Fosberg Fire Weather Index"
     FFWI.attrs["units"] = "[0-100]"
