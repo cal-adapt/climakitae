4b8720673265169a16e1b2149ce2edb116833e8e
Nicole Keeney <nicolejkeeney@gmail.com>
2023-08-07 14:43:02 -0600

    add degF conversions

--- climakitae/unit_conversions.py
+++ climakitae/unit_conversions.py
@@ -112,6 +112,13 @@ def _convert_units(da, selected_units):
             da = da - 273.15
         elif selected_units == "degF":
             da = (1.8 * (da - 273.15)) + 32
+    elif native_units == "degF":
+        # Convert to C
+        if selected_units in ["degC", "K"]:
+            da = (da - 32) / (9 / 5)
+        # Then, if K is selected, convert to K
+        if selected_units == "K":
+            da = da + 273.15
 
     # Fraction/percentage units (relative humidity)
     elif native_units == "[0 to 100]":
