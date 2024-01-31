bbb6eb6b83ccd562522932c77daec253a30a222d
Eric Lehmer <elehmer@berkeley.edu>
2024-01-31 10:48:22 -0800

    Fosberg Derived Index fix where statements reclassing data incorrectly (#308)
    * Remove where from Fosberg to figure out where 100s come from
    
    * Reintro the 1st where statement
    
    * Try to use nodata as filter
    
    * Try to use numeric bottom
    
    * Reset and try again
    
    * Step back one and see
    
    * Use np.clip to keep values in bounds
    
    * Clean up and anno

--- climakitae/tools/indices.py
+++ climakitae/tools/indices.py
@@ -158,13 +158,8 @@ def fosberg_fire_index(t2_F, rh_percent, windspeed_mph):
 
     # Compute the index
     U = windspeed_mph
-    FFWI = (n * ((1 + U**2) ** 0.5)) / 0.3002
-
-    # If fosberg index > 100, reset to 100
-    FFWI = xr.where(FFWI < 100, FFWI, 100, keep_attrs=True)
-
-    # If fosberg index is negative, set to 0
-    FFWI = xr.where(FFWI > 0, FFWI, 0, keep_attrs=True)
+    # If the value falls out of [0-100] range clip the value
+    FFWI = np.clip((n * ((1 + U**2) ** 0.5)) / 0.3002, 0.0, 100.0)
 
     # Reassign coordinate attributes
     # For some reason, these get improperly assigned in the xr.where step
