5e930e49569b20e1c579d6bc9362289e90aca32c
Eric Lehmer <elehmer@berkeley.edu>
2024-04-24 11:18:26 -0700

    Black format

--- climakitae/core/data_interface.py
+++ climakitae/core/data_interface.py
@@ -1179,11 +1179,17 @@ class DataParameters(param.Parameterized):
 
         def warnoflargefilesize(da):
             if da.nbytes >= int(1e9) and da.nbytes < int(5e9):
-                print("! Returned data array is large and operations will take extra time !")
+                print(
+                    "! Returned data array is large and operations will take extra time !"
+                )
             elif da.nbytes >= int(5e9) and da.nbytes < int(1e10):
-                print("!! Returned data array is very large and operations will take considerably more time !!")
+                print(
+                    "!! Returned data array is very large and operations will take considerably more time !!"
+                )
             elif da.nbytes >= int(1e10):
-                print("!!! Returned data array is huge and operations will take vastly more time !!!")
+                print(
+                    "!!! Returned data array is huge and operations will take vastly more time !!!"
+                )
 
         if config is not None:
             if type(config) == str:
