a84b1777c018e7b271445d7ca5652949cb646f0d
claalmve <chencalvin99@berkeley.edu>
2024-05-01 10:00:04 -0700

    Changing dev logger to dev_logging.py and also adding more documentation to the file

--- climakitae/util/dev_logging.py
+++ climakitae/util/dev_logging.py
@@ -1,3 +1,30 @@
+"""
+Notes about this logger (Updated 5/1/24):
+
+This logger is to be used for developers who want to see a comprehensive view of the function call stacks of any functions
+within `climakitae`. In order to use this logger, you must do the following:
+
+    from climakitae.util.dev_logging import _enable_lib_logging, _disable_lib_logging
+    from climakitae import YOUR_MODULE_HERE
+
+    _enable_lib_logging(YOUR_MODULE_HERE)
+    # To disable it after you're done viewing the logger: _disable_lib_logging(YOUR_MODULE_HERE)
+
+Once you've enabled the logger, you should be able to view comprehensive call-stacks and runtimes for functions within that module,
+and other functions within submodules or subclasses within that module. The logger does its best to try and find all `climakitae` functions
+that are within this module or any sub-modules (while avoiding putting repeated wrappers), but it is best practice to wrap each module that
+you are interested in logging separately. I.e.:
+
+    _enable_lib_logging(YOUR_MODULE_HERE_1)
+    _enable_lib_logging(YOUR_MODULE_HERE_2)
+
+
+Some limitations of this logger include:
+    1. If you interrupt function calls that are wrapped with this logger, it will not reset the `indentation_level` of printed calls.
+    What would need to happen in order to resolve this is for you to disable and re-enable the library logger.
+
+"""
+
 import time
 import types
 import importlib
@@ -6,12 +33,12 @@ import importlib
 indentation_level = 0
 
 
-def log(func):
+def _log(func):
     """
     Wraps around existing functions, adding print statements upon execution and the amount of time it takes for execution. Allows function's call-stack to be viewed for all sub-functions that also have this wrapper.
     """
 
-    def wrapper(*args, **kwargs):
+    def _wrapper(*args, **kwargs):
         """Wraps timer and print statement around functions if `lib_log_enabled` is True, otherwise
         just return the function's result."""
         global indentation_level
@@ -26,12 +53,11 @@ def log(func):
             + f"Execution time for {func.__name__}: {end_time - start_time:.4g}"
         )
         return results
-        # return func(*args, **kwargs)
 
-    return wrapper
+    return _wrapper
 
 
-def enable_lib_logging(obj):
+def _enable_lib_logging(obj):
     """
     Adds the `log` wrapper to all functions and sub-classes within the given module or class.
 
@@ -57,7 +83,7 @@ def enable_lib_logging(obj):
 
                     # Only add loggers to functions within AE
                     if "climakitae" in res.__module__:
-                        setattr(obj, name, log(res))
+                        setattr(obj, name, _log(res))
 
                 # Check if the object is a class type, is not the literal string '__class__', and is created within climakitae.
                 elif (
@@ -66,12 +92,12 @@ def enable_lib_logging(obj):
                     and res.__module__[:10] == "climakitae"
                 ):
                     # Recursively add logging wrapper to any classes within the passed in module/class.
-                    enable_lib_logging(res)
+                    _enable_lib_logging(res)
     else:
         print("Error: Current object is not a module object.")
 
 
-def disable_lib_logging(module):
+def _disable_lib_logging(module):
     """
     Removes the `log` wrapper to all functions within the given module.
     """
@@ -79,21 +105,3 @@ def disable_lib_logging(module):
     # I have tried to reference a `__wrapped__` attribute on wrapped functions, but they don't seem to exist.
     importlib.reload(module)
     return
-
-
-# def enable_lib_logging():
-#     """
-#     Enables library logging.
-#     """
-#     global lib_log_enabled
-#     if not lib_log_enabled:
-#         lib_log_enabled = True
-
-
-# def disable_lib_logging():
-#     """
-#     Disables library logging.
-#     """
-#     global lib_log_enabled
-#     if lib_log_enabled:
-#         lib_log_enabled = False
