436a2f47a0ded49986f69794705c02c746656561
Eric Lehmer <elehmer@berkeley.edu>
2024-07-01 11:13:07 -0700

    Boundaries documentation update

--- climakitae/core/boundaries.py
+++ climakitae/core/boundaries.py
@@ -5,8 +5,10 @@ class Boundaries:
     """Get geospatial polygon data from the S3 stored parquet catalog.
     Used to access boundaries for subsetting data by state, county, etc.
 
-    Parameters
-    -----------
+    Attributes
+    ----------
+    _cat: intake.catalog.Catalog
+        Parquet boundary catalog instance
     _us_states: pd.DataFrame
         Table of US state names and geometries
     _ca_counties: pd.DataFrame
@@ -21,7 +23,26 @@ class Boundaries:
         Table of California Demand Forecast Zones
     _ca_electric_balancing_areas: pd.DataFrame
         Table of Electric Balancing Areas
+    Methods
+    -------
+    load(self)
+        Reads parquet files and sets class attributes
+    _get_us_states(self)
+        Returns a dict of state abbreviations and indices
+    _get_ca_counties(self)
+        Returns a dict of California counties and their indices
+    _get_ca_watersheds(self)
+        Returns a dict for CA watersheds and their indices
+    _get_forecast_zones(self)
+        Returns a dict for CA electricity demand forecast zones
+    _get_ious_pous(self)
+        Returns a dict for CA electric load serving entities IOUs & POUs
+    _get_electric_balancing_areas(self)
+        Returns a dict for CA Electric Balancing Authority Areas
+    boundary_dict(self)
+        Returns a dict of the other boundary dicts, used to populate ck.Select
     """
+
 
     _cat = None
     _us_states = None
@@ -120,7 +141,7 @@ class Boundaries:
 
     def _get_forecast_zones(self):
         """
-        Returns a lookup dictionary for CA watersheds that references
+        Returns a lookup dictionary for CA Electricity Demand Forecast Zones that references
         the geoparquet file.
 
         Returns
@@ -134,7 +155,7 @@ class Boundaries:
 
     def _get_ious_pous(self):
         """
-        Returns a lookup dictionary for IOUs & POUs that references
+        Returns a lookup dictionary for CA Electric Load Serving Entities IOUs & POUs that references
         the geoparquet file.
 
         Returns
