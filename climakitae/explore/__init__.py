49d657a38874a7b59e2c55cbfdce04635320e95f
Eric Lehmer <elehmer@berkeley.edu>
2024-07-23 11:42:08 -0700

    Black format

--- climakitae/explore/__init__.py
+++ climakitae/explore/__init__.py
@@ -2,6 +2,5 @@ from climakitae.explore.warming import WarmingLevels
 
 
 def warming_levels():
-    """Top level alias for the WarmingLevels class. Typical way to call class.
-    """
+    """Top level alias for the WarmingLevels class. Typical way to call class."""
     return WarmingLevels()
