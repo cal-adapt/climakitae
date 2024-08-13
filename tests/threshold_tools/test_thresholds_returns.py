8bb527134986edc75ba0a6829fae091f98b63648
Eric Lehmer <elehmer@berkeley.edu>
2024-08-13 11:54:59 -0700

    Readd tests that were moved to climakitaegui

---
+++ tests/threshold_tools/test_thresholds_returns.py
@@ -0,0 +1,162 @@
+"""
+Test the available extreme value theory functions for calculating return
+values and periods. These tests do not check the correctness of the
+calculations; they just ensure that the functions run without error, or raise
+the expected error messages for invalid argument specifications.
+"""
+
+import os
+import numpy as np
+import pytest
+import xarray as xr
+
+from climakitae.explore import threshold_tools
+
+# ------------- Data for testing -----------------------------------------------
+
+
+# Generate an annual maximum series (ams) datarray for testing
+@pytest.fixture
+def T2_ams(rootdir):
+    # This data is generated in "create_test_data.py"
+    test_filename = "test_data/timeseries_data_T2_2014_2016_monthly_45km.nc"
+    test_filepath = os.path.join(rootdir, test_filename)
+    test_data = (
+        xr.open_dataset(test_filepath)
+        .T2.mean(dim=("x", "y"), skipna=True)
+        .isel(scenario=0, simulation=0)
+    )
+    return threshold_tools.get_block_maxima(test_data, block_size=1, check_ess=False)
+
+
+# ------------- Test return values and periods ----------------------------------
+
+
+# Test Return Values
+def test_return_value(T2_ams):
+    rvs = threshold_tools.get_return_value(
+        T2_ams, return_period=10, distr="gev", bootstrap_runs=1, multiple_points=False
+    )
+    assert not np.isnan(rvs["return_value"].values[()])
+
+
+# Test invalid distribution argument for Return Values
+def test_return_value_invalid_distr(T2_ams):
+    with pytest.raises(ValueError, match="invalid distribution type"):
+        rvs = threshold_tools.get_return_value(
+            T2_ams,
+            return_period=10,
+            distr="foo",
+            bootstrap_runs=1,
+            multiple_points=False,
+        )
+
+
+# Test Return Periods
+def test_return_period(T2_ams):
+    rvs = threshold_tools.get_return_period(
+        T2_ams,
+        return_value=290,
+        distr="gumbel",
+        bootstrap_runs=1,
+        multiple_points=False,
+    )
+    assert not np.isnan(rvs["return_period"].values[()])
+
+
+# Test invalid distribution argument for Return Periods
+def test_return_period_invalid_distr(T2_ams):
+    with pytest.raises(ValueError, match="invalid distribution type"):
+        rvs = threshold_tools.get_return_period(
+            T2_ams,
+            return_value=290,
+            distr="foo",
+            bootstrap_runs=1,
+            multiple_points=False,
+        )
+
+
+# Test return values for different block sizes
+def test_return_values_block_size(T2_ams):
+    rvs1 = threshold_tools.get_return_value(
+        T2_ams, return_period=10, distr="gev", bootstrap_runs=1, multiple_points=False
+    )
+    # set different block size attribute to test that the calculation is handled differently:
+    T2_ams.attrs["block size"] = "2 year"
+    rvs2 = threshold_tools.get_return_value(
+        T2_ams, return_period=10, distr="gev", bootstrap_runs=1, multiple_points=False
+    )
+    # test that the return values from longer block sizes should be smaller:
+    assert rvs1["return_value"].values[()] >= rvs2["return_value"].values[()]
+
+
+# Test return periods for different block sizes
+def test_return_periods_block_size(T2_ams):
+    rps1 = threshold_tools.get_return_period(
+        T2_ams, return_value=290, distr="gev", bootstrap_runs=1, multiple_points=False
+    )
+    # set different block size attribute to test that the calculation is handled differently:
+    T2_ams.attrs["block size"] = "2 year"
+    rps2 = threshold_tools.get_return_period(
+        T2_ams, return_value=290, distr="gev", bootstrap_runs=1, multiple_points=False
+    )
+    # test that the return periods from longer block sizes should be larger:
+    assert rps1["return_period"].values[()] <= rps2["return_period"].values[()]
+
+
+# Test return probabilities for different block sizes
+def test_return_probs_block_size(T2_ams):
+    rps1 = threshold_tools.get_return_prob(
+        T2_ams, threshold=290, distr="gev", bootstrap_runs=1, multiple_points=False
+    )
+    # set different block size attribute to test that the calculation is handled differently:
+    T2_ams.attrs["block size"] = "2 year"
+    rps2 = threshold_tools.get_return_prob(
+        T2_ams, threshold=290, distr="gev", bootstrap_runs=1, multiple_points=False
+    )
+    # test that the return probs from longer block sizes should be smaller:
+    assert rps1["return_prob"].values[()] >= rps2["return_prob"].values[()]
+
+
+# -------------- Test AMS block maxima calculations for complex extreme events
+
+
+# Test that the AMS (block maxima) for a 3-day grouped event are lower than
+# the simple AMS (single hottest value in each year)
+def test_ams_ex1(T2_hourly):
+    T2_hourly = T2_hourly.isel(scenario=0, simulation=0)
+    ams = threshold_tools.get_block_maxima(T2_hourly, check_ess=False)
+    ams_3d = threshold_tools.get_block_maxima(
+        T2_hourly, groupby=(1, "day"), grouped_duration=(3, "day"), check_ess=False
+    )
+    assert (ams >= ams_3d).all()
+
+
+# Test that the AMS (block maxima) for a 3-day continous event are lower than
+# the AMS for a grouped 3-day event
+def test_ams_ex2(T2_hourly):
+    T2_hourly = T2_hourly.isel(scenario=0, simulation=0)
+    ams_3d = threshold_tools.get_block_maxima(
+        T2_hourly, groupby=(1, "day"), grouped_duration=(3, "day"), check_ess=False
+    )
+    ams_72h = threshold_tools.get_block_maxima(
+        T2_hourly, duration=(72, "hour"), check_ess=False
+    )
+    assert (ams_3d >= ams_72h).all()
+
+
+# Test that the AMS (block maxima) for a 4-hour per day for 3 days are lower
+# than the AMS for a grouped 3-day event
+def test_ams_ex3(T2_hourly):
+    T2_hourly = T2_hourly.isel(scenario=0, simulation=0)
+    ams_3d = threshold_tools.get_block_maxima(
+        T2_hourly, groupby=(1, "day"), grouped_duration=(3, "day"), check_ess=False
+    )
+    ams_3d_4h = threshold_tools.get_block_maxima(
+        T2_hourly,
+        duration=(4, "hour"),
+        groupby=(1, "day"),
+        grouped_duration=(3, "day"),
+        check_ess=False,
+    )
+    assert (ams_3d >= ams_3d_4h).all()
---
+++ tests/timeseriestools/test_timeseriestools_monthly.py
@@ -0,0 +1,133 @@
+"""
+This script contains tests on various Timeseries Tools options using monthly 
+data. For now, the tests only test that the various parameter combinations can 
+run through `transform_data` without error, and that the data has been transformed
+(not equal to original values), but does not test for exact expected values.
+"""
+
+import climakitae.explore.timeseries as tst
+import datetime as dt
+import pytest
+import os
+import xarray as xr
+import numpy as np
+
+# -------- Read in the test dataset and return a TimeSeriesParams object -------
+
+
+@pytest.fixture
+def test_TSP(rootdir):
+    # This data is generated in "create_timeseries_test_data.py"
+    test_filename = "test_data/timeseries_data_T2_2014_2016_monthly_45km.nc"
+    test_filepath = os.path.join(rootdir, test_filename)
+    test_data = xr.open_dataset(test_filepath).T2
+
+    # Compute area average
+    weights = np.cos(np.deg2rad(test_data.lat))
+    test_data = test_data.weighted(weights).mean("x").mean("y")
+
+    ts = tst.TimeSeries(test_data)  # make Timeseries object
+    return ts.choices  # return the underlying TimeSeriesParams object for testing
+
+
+def test_monthly_smoothing(test_TSP):
+    # Specify Params options
+    test_TSP.smoothing = "Running Mean"
+    test_TSP.num_timesteps = 3
+    test_TSP.anomaly = False
+
+    # Transform data and test
+    result = test_TSP.transform_data()  # transform_data calls _running_mean()
+    assert (result == test_TSP.data).sum().values.item() == 0
+
+
+# ------------- Test monthly weighted anomaly ----------------------------------
+
+
+def test_monthly_anomaly(test_TSP):
+    # Specify Params options
+    test_TSP.anomaly = True
+    test_TSP.reference_range = (dt.datetime(2014, 1, 1), dt.datetime(2014, 12, 31))
+
+    # Transform data and test
+    result = test_TSP.transform_data()
+    assert (result == test_TSP.data).sum().values.item() == 0
+
+
+# ------------- Test anomaly and smoothing together ----------------------------
+
+
+def test_monthly_anomaly_and_smoothing(test_TSP):
+    # Specify Params options
+    test_TSP.smoothing = "Running Mean"
+    test_TSP.num_timesteps = 3
+    test_TSP.anomaly = True
+    test_TSP.reference_range = (dt.datetime(2014, 1, 1), dt.datetime(2014, 12, 31))
+
+    # Transform data and test
+    result = test_TSP.transform_data()
+    assert (result == test_TSP.data).sum().values.item() == 0
+
+
+# ------------- Test seasonal cycle removal w/ and w/o smoothing ---------------
+
+
+def test_seasonal(test_TSP):
+    # Specify Params options
+    test_TSP.anomaly = False
+    test_TSP.remove_seasonal_cycle = True
+
+    # Transform data and test
+    result = test_TSP.transform_data()
+    assert (result == test_TSP.data).sum().values.item() == 0
+
+
+def test_seasonal_and_smoothing(test_TSP):
+    # Specify Params options
+    test_TSP.smoothing = "Running Mean"
+    test_TSP.num_timesteps = 3
+    test_TSP.anomaly = False
+    test_TSP.remove_seasonal_cycle = True
+
+    # Transform data and test
+    result = test_TSP.transform_data()
+    assert (result == test_TSP.data).sum().values.item() == 0
+
+
+# ------------- Test extremes options ------------------------------------------
+
+
+def test_extremes_smoothing(test_TSP):
+    # Specify Params options
+    test_TSP.anomaly = False
+    test_TSP.smoothing = "Running Mean"
+    test_TSP.num_timesteps = 3
+    test_TSP.extremes = ["Min"]
+    test_TSP.resample_window = 2
+
+    # Transform data and test
+    result = test_TSP.transform_data()
+    assert (result == test_TSP.data).sum().values.item() == 0
+
+
+def test_extremes_min(test_TSP):
+    # Specify Params options
+    test_TSP.anomaly = False
+    test_TSP.extremes = ["Min"]
+    test_TSP.resample_window = 2
+
+    # Transform data and test
+    result = test_TSP.transform_data()
+    assert (result == test_TSP.data).sum().values.item() == 0
+
+
+def test_extremes_percentile(test_TSP):
+    # Specify Params options
+    test_TSP.anomaly = False
+    test_TSP.extremes = ["Percentile"]
+    test_TSP.resample_window = 2
+    test_TSP.percentile = 0.95
+
+    # Transform data and test
+    result = test_TSP.transform_data()
+    assert (result == test_TSP.data).sum().values.item() == 0
