c5af7cdc5823675200c6dfbaf771471dbbd2586c
Eric Lehmer <elehmer@berkeley.edu>
2024-08-19 10:07:50 -0700

    Move plotting utils to climakitaegui

--- climakitae/util/utils.py
+++ climakitae/util/utils.py
@@ -533,47 +533,6 @@ def combine_hdd_cdd(data):
     return data
 
 
-def hdd_cdd_lineplot(annual_data, trendline, title="title"):
-    """Plots annual CDD/HDD with trendline provided
-
-    Parameters
-    ----------
-    annual_data: xr.DataArray
-    trendline: xr.Dataset
-    title: str
-
-    Returns
-    -------
-    data: hvplot.line
-    """
-    return annual_data.hvplot.line(
-        x="year",
-        by="simulation",
-        width=800,
-        height=350,
-        title=title,
-        yformatter="%.0f",  # Remove scientific notation
-    ) * trendline.hvplot.line(  # Add trendline
-        x="year", color="black", line_dash="dashed", label="trendline"
-    )
-
-
-def hdh_cdh_lineplot(data):
-    """Plots HDH/CDH
-
-    Parameters
-    ----------
-    data: xr.DataArray
-
-    Returns
-    -------
-    data: hvplot.line
-    """
-    return data.hvplot.line(
-        x="time", by="simulation", title=data.name, ylabel=data.name + " (degF)"
-    )
-
-
 def summary_table(data):
     """Helper function to organize dataset object into a pandas dataframe for ease.
 
