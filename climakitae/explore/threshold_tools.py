fe9a864e5666afd11772d5abeaf7cb2eabbf432f
Victoria Ford <fordvecellio@gmail.com>
2024-12-18 16:49:41 -0600

    testing with fillna(value=0)

--- climakitae/explore/threshold_tools.py
+++ climakitae/explore/threshold_tools.py
@@ -112,7 +112,7 @@ def get_block_maxima(
 
         # select the max (min) in each group
         if extremes_type == "max":
-            da_series = da_series.resample(time=f"{group_len}D", label="left").max(skipna=True, min_count=1)
+            da_series = da_series.resample(time=f"{group_len}D", label="left").fillna(value=0).max()
             print("2", da_series.values.max()) # testing
         elif extremes_type == "min":
             da_series = da_series.resample(time=f"{group_len}D", label="left").min()
@@ -138,7 +138,7 @@ def get_block_maxima(
 
     # Now select the most extreme value for each block in the series
     if extremes_type == "max":
-        bms = da_series.resample(time=f"{block_size}A").max(keep_attrs=True, skipna=True, min_count=1)
+        bms = da_series.resample(time=f"{block_size}A").fillna(value=0).max(keep_attrs=True)
         bms.attrs["extremes type"] = "maxima"
         print("4", bms.values.max()) # testing
     elif extremes_type == "min":
