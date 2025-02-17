08f80ef456a24b1b5682fd47299406e4d4192ed4
Eric Lehmer <elehmer@berkeley.edu>
2025-02-16 22:01:49 -0800

    Pypi requires README context type to be specified

--- setup.py
+++ setup.py
@@ -1,4 +1,14 @@
 #!/usr/bin/env python
 from setuptools import setup
 
-setup()
+# read the contents of your README file
+from pathlib import Path
+this_directory = Path(__file__).parent
+long_description = (this_directory / "README.md").read_text()
+
+setup(
+    name='climakitae',
+    # other arguments omitted
+    long_description=long_description,
+    long_description_content_type='text/markdown'
+)
