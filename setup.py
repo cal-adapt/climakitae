84a70500b89636c5f941f6c91b772522aac20e38
Eric Lehmer <elehmer@berkeley.edu>
2025-02-16 22:10:00 -0800

    Black format

--- setup.py
+++ setup.py
@@ -3,12 +3,13 @@ from setuptools import setup
 
 # read the contents of your README file
 from pathlib import Path
+
 this_directory = Path(__file__).parent
 long_description = (this_directory / "README.md").read_text()
 
 setup(
-    name='climakitae',
+    name="climakitae",
     # other arguments omitted
     long_description=long_description,
-    long_description_content_type='text/markdown'
+    long_description_content_type="text/markdown",
 )
