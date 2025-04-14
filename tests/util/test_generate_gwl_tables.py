e0e13e8c6055eb894916fa850068e552384bb594
Ana Ordonez <ana.ordonez@eaglerockanalytics.com>
2025-04-14 09:54:55 -0700

    remove unneeded code and add more model checks

--- tests/util/test_generate_gwl_tables.py
+++ tests/util/test_generate_gwl_tables.py
@@ -9,16 +9,14 @@ from climakitae.util.generate_gwl_tables import get_sims_on_aws
 def test_get_sims_on_aws():
     """Check that expected scenarios and models are returned."""
     df = pd.read_csv("https://cmip6-pds.s3.amazonaws.com/pangeo-cmip6.csv")
-    df_subset = df[
-        (df.table_id == "Amon")
-        & (df.variable_id == "tas")
-        & (df.experiment_id == "historical")
-    ]
     sims_on_aws = get_sims_on_aws(df)
-    rows = sims_on_aws.index.tolist()
     cols = sims_on_aws.columns.tolist()
 
     for scenario in ["historical", "ssp585", "ssp370", "ssp245", "ssp126"]:
         assert scenario in cols
 
     assert sims_on_aws["historical"]["TaiESM1"] == ["r1i1p1f1"]
+    assert sims_on_aws["ssp585"]["MPI-ESM1-2-HR"] == ["r1i1p1f1", "r2i1p1f1"]
+    assert sims_on_aws["ssp370"]["GFDL-CM4"] == []
+    assert len(sims_on_aws["ssp245"]["ACCESS-ESM1-5"]) == 10
+    assert sims_on_aws["ssp126"]["CESM2"] == ["r11i1p1f1", "r4i1p1f1", "r10i1p1f1"]
