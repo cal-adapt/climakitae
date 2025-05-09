ca4f1111cec41e82c954c854581b5ee445ceebf6
Ana Ordonez <ana.ordonez@eaglerockanalytics.com>
2025-05-09 14:04:32 -0700

    add tests

--- tests/threshold_tools/test_threshold_tools.py
+++ tests/threshold_tools/test_threshold_tools.py
@@ -9,7 +9,10 @@ import xarray as xr
 from climakitae.explore import threshold_tools
 from climakitae.explore.threshold_tools import (
     _calc_average_ess_gridded_data,
+    _calc_average_ess_timeseries_data,
+    _exceedance_count_name,
     _get_distr_func,
+    _get_exceedance_events,
     _get_fitted_distr,
 )
 
@@ -40,6 +43,18 @@ class TestThresholdTools:
             },
         )
         result = _calc_average_ess_gridded_data(test, 1)
+        assert not np.isnan(result)
+        assert isinstance(result, float)
+
+    def test__calc_average_ess_timeseries_data(self):
+        test_data = test_data = np.random.rand(365 * 3) * 100
+        test = xr.DataArray(
+            data=test_data,
+            coords={
+                "time": pd.date_range(start="2001-01-01", end="2003-12-31"),
+            },
+        )
+        result = _calc_average_ess_timeseries_data(test, 1)
         assert not np.isnan(result)
         assert isinstance(result, float)
 
@@ -78,3 +93,82 @@ class TestThresholdTools:
 
         with pytest.raises(ValueError):
             _get_distr_func("")
+
+    def test__exceedance_count_name(self):
+        item1 = 4
+        item2 = "day"
+        test = xr.DataArray()
+        test.attrs["duration2"] = (item1, item2)
+        result = _exceedance_count_name(test)
+        assert result == "Number of {0}-{1} events".format(item1, item2)
+
+        test = xr.DataArray()
+        test.attrs["duration2"] = None
+        test.attrs["group"] = (item1, item2)
+        result = _exceedance_count_name(test)
+        assert result == "Number of {0}-{1} events".format(item1, item2)
+
+        test = xr.DataArray()
+        test.attrs["duration2"] = None
+        test.attrs["group"] = None
+        tscales = {"hourly": "hours", "monthly": "months", "daily": "days"}
+        for item in tscales:
+            test.attrs["frequency"] = item
+            result = _exceedance_count_name(test)
+            assert result == "Number of {0}".format(tscales[item])
+
+    def test__get_exceedance_events(self):
+        test_data = test_data = np.random.rand(10, 10, 365 * 3) * 100
+        test_data[0, 0, 0] = 1  # making sure something is <10
+        test = xr.DataArray(
+            data=test_data,
+            coords={
+                "x": np.arange(10, 20, 1),
+                "y": np.arange(10, 20, 1),
+                "time": pd.date_range(start="2001-01-01", end="2003-12-31"),
+            },
+        )
+        result = _get_exceedance_events(test, 10, threshold_direction="below")
+        assert result.sum().data == xr.where(test < 10, 1, 0).sum().data
+
+        with pytest.raises(ValueError):
+            _get_exceedance_events(test, 10, threshold_direction="belw")
+
+        with pytest.raises(ValueError):
+            _get_exceedance_events(test, 90, duration1=(3, "month"))
+
+
+@pytest.mark.advanced
+class TestKsStat:
+    def test_ks_stat(self):
+        # Not verifying results, just checking that it runs
+        test_data = test_data = np.random.rand(10, 10, 365 * 3) * 100
+        test = xr.DataArray(
+            data=test_data,
+            coords={
+                "x": np.arange(10, 20, 1),
+                "y": np.arange(10, 20, 1),
+                "time": pd.date_range(start="2001-01-01", end="2003-12-31"),
+            },
+        )
+        result = threshold_tools.get_ks_stat(test)
+        assert isinstance(result, xr.Dataset)
+        assert result.distribution == "gev"
+        assert "d_statistic" in result
+        assert "p_value" in result
+
+        # Check that other distributions run
+        result = threshold_tools.get_ks_stat(test, "gumbel")
+        assert isinstance(result, xr.Dataset)
+
+        result = threshold_tools.get_ks_stat(test, "weibull")
+        assert isinstance(result, xr.Dataset)
+
+        result = threshold_tools.get_ks_stat(test, "pearson3")
+        assert isinstance(result, xr.Dataset)
+
+        result = threshold_tools.get_ks_stat(test, "genpareto")
+        assert isinstance(result, xr.Dataset)
+
+        result = threshold_tools.get_ks_stat(test, "gamma")
+        assert isinstance(result, xr.Dataset)
