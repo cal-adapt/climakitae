faa1ad4f8f9c6db90adbf057d77849b9090a334f
Eric Lehmer <elehmer@berkeley.edu>
2025-05-16 09:26:49 -0700

    use match instead of elif in convert_units

--- climakitae/util/unit_conversions.py
+++ climakitae/util/unit_conversions.py
@@ -70,84 +70,92 @@ def convert_units(da: xr.DataArray, selected_units: str) -> xr.DataArray:
         native_units = "Pa"
 
     # Pass if chosen units is the same as native units
-    if native_units == selected_units:
-        return da
+    match native_units:
+        case selected_units:
+            return da
 
-    # Precipitation units
-    elif native_units in ["mm", "mm/d", "mm/h"]:
-        if selected_units in ["inches", "inches/h", "inches/d"]:
-            da = da / 25.4
-        elif selected_units == "kg m-2 s-1":
-            da = da / 86400
+        # Precipitation units
+        case "mm" | "mm/d" | "mm/h":
+            match selected_units:
+                case "inches" | "inches/h" | "inches/d":
+                    da = da / 25.4
+                case "kg m-2 s-1":
+                    da = da / 86400
+                    if da.attrs["frequency"] == "monthly":
+                        da_name = da.name
+                        da = da / da["time"].dt.days_in_month
+                        da.name = da_name  # Name is lost during computation above
+        case "kg m-2 s-1":
             if da.attrs["frequency"] == "monthly":
                 da_name = da.name
-                da = da / da["time"].dt.days_in_month
+                da = da * da["time"].dt.days_in_month
                 da.name = da_name  # Name is lost during computation above
-    elif native_units == "kg m-2 s-1":
-        if da.attrs["frequency"] == "monthly":
-            da_name = da.name
-            da = da * da["time"].dt.days_in_month
-            da.name = da_name  # Name is lost during computation above
-        if selected_units == "mm":
-            da = da * 86400
-        elif selected_units == "inches":
-            da = (da * 86400) / 25.4
+            match selected_units:
+                case "mm":
+                    da = da * 86400
+                case "inches":
+                    da = (da * 86400) / 25.4
 
-    # Moisture ratio units
-    elif native_units in ["kg/kg", "kg kg-1"]:
-        if selected_units in ["g/kg", "g kg-1"]:
-            da = da * 1000
+        # Moisture ratio units
+        case "kg/kg" | "kg kg-1":
+            if selected_units in ["g/kg", "g kg-1"]:
+                da = da * 1000
 
-    # Specific humidity
-    elif native_units == "g/kg":
-        if selected_units == "kg/kg":
-            da = da / 1000
+        # Specific humidity
+        case "g/kg":
+            if selected_units == "kg/kg":
+                da = da / 1000
 
-    # Specific humidity
-    elif native_units == "g/kg":
-        if selected_units == "kg/kg":
-            da = da / 1000
-            da.attrs["units"] = selected_units
+        # Specific humidity
+        case "g/kg":
+            if selected_units == "kg/kg":
+                da = da / 1000
+                da.attrs["units"] = selected_units
 
-    # Pressure units
-    elif native_units == "Pa":
-        if selected_units == "hPa":
-            da = da / 100.0
-        elif selected_units == "mb":
-            da = da / 100.0
-        elif selected_units == "inHg":
-            da = da * 0.000295300
+        # Pressure units
+        case "Pa":
+            match selected_units:
+                case "hPa":
+                    da = da / 100.0
+                case "mb":
+                    da = da / 100.0
+                case "inHg":
+                    da = da * 0.000295300
 
-    # Wind units
-    elif native_units in ["m/s", "m s-1"]:
-        if selected_units == "knots":
-            da = da * 1.9438445
-        elif selected_units == "mph":
-            da = da * 2.236936
+        # Wind units
+        case "m/s" | "m s-1":
+            match selected_units:
+                case "knots":
+                    da = da * 1.9438445
+                case "mph":
+                    da = da * 2.236936
 
-    # Temperature units
-    elif native_units == "K":
-        if selected_units == "degC":
-            da = da - 273.15
-        elif selected_units == "degF":
-            da = (1.8 * (da - 273.15)) + 32
-    elif native_units == "degC":
-        if selected_units == "K":
-            da = da + 273.15
-        elif selected_units == "degF":
-            da = (1.8 * da) + 32
-    elif native_units == "degF":
-        # Convert to C
-        if selected_units in ["degC", "K"]:
-            da = (da - 32) / 1.8
-        # Then, if K is selected, convert to K
-        if selected_units == "K":
-            da = da + 273.15
+        # Temperature units
+        case "K":
+            match selected_units:
+                case "degC":
+                    da = da - 273.15
+                case "degF":
+                    da = (1.8 * (da - 273.15)) + 32
+        case "degC":
+            match selected_units:
+                case "K":
+                    da = da + 273.15
+                case "degF":
+                    da = (1.8 * da) + 32
+        case "degF":
+            # Convert to C
+            match selected_units:
+                case "degC" | "K":
+                    da = (da - 32) / 1.8
+                # Then, if K is selected, convert to K
+                case "K":
+                    da = da + 273.15
 
-    # Fraction/percentage units (relative humidity)
-    elif native_units == "[0 to 100]":
-        if selected_units == "fraction":
-            da = da / 100.0
+        # Fraction/percentage units (relative humidity)
+        case "[0 to 100]":
+            if selected_units == "fraction":
+                da = da / 100.0
 
     # Update unit attribute to reflect converted unit
     da.attrs["units"] = selected_units
