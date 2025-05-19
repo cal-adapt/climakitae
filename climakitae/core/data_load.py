405c87271b44e0234c4ce1a428c04ffc120d210b
Eric Lehmer <elehmer@berkeley.edu>
2025-05-19 11:41:01 -0700

    Remove case _ that is incorrect

--- climakitae/core/data_load.py
+++ climakitae/core/data_load.py
@@ -446,8 +446,6 @@ def _override_unit_defaults(da: xr.DataArray, var_id: str) -> xr.DataArray:
             # rsds units are "W m-2"
             # rename them to W/m2 to match the lookup catalog, and the units for WRF radiation variables
             da.attrs["units"] = "W/m2"
-        case _:
-            raise ValueError('var_id must be either "huss" or "rsds"')
     return da
 
 
