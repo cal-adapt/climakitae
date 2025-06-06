e09c779b19bf2034be61c0a5579e86f4095529b8
neilSchroeder <neil.raymond.schroeder@gmail.com>
2025-06-05 20:07:15 -0500

    Add Localize validator and processor to param validation and processors

--- climakitae/new_core/param_validation/__init__.py
+++ climakitae/new_core/param_validation/__init__.py
@@ -4,4 +4,5 @@ Import param validation classes ensuring they are registered.
 
 from .clip_param_validator import validate_clip_param
 from .data_param_validator import DataValidator
+from .localize_param_validator import validate_localize_param
 from .renewables_param_validator import RenewablesValidator
--- climakitae/new_core/processors/__init__.py
+++ climakitae/new_core/processors/__init__.py
@@ -6,5 +6,6 @@ from .clip import Clip
 from .concatenate import Concat
 from .convert_units import ConvertUnits
 from .filter_unbiased_models import FilterUnbiasedModels
+from .localize import Localize
 from .time_slice import TimeSlice
 from .update_attributes import UpdateAttributes
