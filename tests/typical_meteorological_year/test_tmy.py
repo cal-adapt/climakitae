11decf4e22b282fc6d021cdd54c8d33eb594f455
Ana Ordonez <ana.ordonez@eaglerockanalytics.com>
2025-08-29 23:26:58 +0000

    add tests, 78% coverage

--- tests/typical_meteorological_year/test_tmy.py
+++ tests/typical_meteorological_year/test_tmy.py
@@ -10,12 +10,12 @@ from climakitae.core.constants import UNSET
 from climakitae.explore.typical_meteorological_year import (
     TMY,
     compute_cdf,
+    get_cdf_monthly,
     compute_weighted_fs,
     fs_statistic,
     get_cdf,
     get_cdf_by_mon_and_sim,
     get_cdf_by_sim,
-    plot_one_var_cdf,
 )
 
 
@@ -36,10 +36,27 @@ class TestFunctionsForTMY:
         assert result[0].max() == pytest.approx(test_data.max(), abs=1e-6)
         assert result[1][-1] == pytest.approx(1.0, abs=1e-6)  # Max probability 1
 
-    def test_set_cdf_monthly(self):
+    def test_get_cdf_monthly(self):
         """Test CDF monthly calculation."""
-        pass
-        # TODO: write test
+        test_data = np.arange(0, 365 * 3 * 24, 1)
+        test_data = np.expand_dims(test_data, [1]) * np.ones((1, 2))
+        coords = {
+            "time": pd.date_range(
+                start="2001-01-01-00", end="2003-12-31-23", freq="1h"
+            ),
+            "simulation": ["sim1", "sim2"],
+        }
+        da = xr.DataArray(
+            name="Air Temperature at 2m",
+            dims=["time", "simulation"],
+            data=test_data,
+            coords=coords,
+        ).to_dataset()
+        result = get_cdf_monthly(da)
+        assert isinstance(result, xr.Dataset)
+        # Was cdf applied over simulation and months?
+        for dim in ["year", "month", "bin_number"]:
+            assert dim in result.dims
 
     def test_get_cdf_by_sim(self):
         """Test cdf computation by simulation."""
@@ -192,41 +209,136 @@ class TestFunctionsForTMY:
         for variable, value in zip(vars_list, values_list):
             assert fs[variable] == value
 
-    def test_plot_one_var_cdf(self):
-        """Test that plot runs and returns object."""
-        # Create test dataset
-        test_data = np.arange(0, 365 * 3, 1)
-        test_data = test_data * np.ones((2, len(test_data)))
-        test = xr.DataArray(
-            name="temperature",
-            data=test_data,
-            coords={
-                "simulation": ["sim1", "sim2"],
-                "time": pd.date_range(start="2001-01-01", end="2003-12-31"),
-            },
-        ).to_dataset()
-        test["temperature"].attrs["units"] = "C"
-        result = get_cdf(test)
 
-        test_plot = plot_one_var_cdf(result, "temperature")
-
-        assert isinstance(test_plot, panel.layout.base.Column)
-
-
-def mock_da_daily():
-    test_data = np.arange(0, 365 * 3, 1)
-    test_data = test_data * np.ones((2, len(test_data)))
-    test = xr.DataArray(
-        name="temperature",
+@pytest.fixture
+def mock_t_hourly():
+    test_data = np.arange(0, 365 * 3 * 24, 1)
+    test_data = np.expand_dims(test_data, [1, 2])
+    coords = {
+        "x": 7.819e05,
+        "y": -4.116e06,
+        "lakemask": 0,
+        "landmask": 0,
+        "Lambert_Conformal": 0,
+        "time": pd.date_range(start="2001-01-01-00", end="2003-12-31-23", freq="1h"),
+        "scenario": ["Historical + SSP 3-7.0"],
+        "simulation": ["WRF_EC-Earth3_r1i1p1f1"],
+    }
+    da = xr.DataArray(
+        name="Air Temperature at 2m",
+        dims=["time", "scenario", "simulation"],
         data=test_data,
-        coords={
-            "simulation": ["sim1", "sim2"],
-            "time": pd.date_range(
-                start="2001-01-01-00", end="2003-12-31-23", freq="1D"
-            ),
-        },
+        coords=coords,
     )
-    return test
+    da.attrs = {
+        "variable_id": "t2",
+        "extended_description": "Temperature of the air 2m above Earth's surface.",
+        "units": "degC",
+        "data_type": "Gridded",
+        "resolution": "9 km",
+        "frequency": "hourly",
+        "location_subset": ["coordinate selection"],
+        "approach": "Time",
+        "downscaling_method": "Dynamical",
+        "institution": "UCLA",
+        "grid_mapping": "Lambert_Conformal",
+        "timezone": "America/Los_Angeles",
+    }
+    return da
+
+
+def mock_t_ds():
+    test_data = np.arange(0, 365 * 3 * 24, 1)
+    test_data = np.expand_dims(test_data, [1, 2])
+    coords = {
+        "x": 7.819e05,
+        "y": -4.116e06,
+        "lakemask": 0,
+        "landmask": 0,
+        "Lambert_Conformal": 0,
+        "time": pd.date_range(start="2001-01-01-00", end="2003-12-31-23", freq="1h"),
+        "scenario": ["Historical + SSP 3-7.0"],
+        "simulation": ["WRF_EC-Earth3_r1i1p1f1"],
+    }
+    da = xr.DataArray(
+        name="Air Temperature at 2m",
+        dims=["time", "scenario", "simulation"],
+        data=test_data,
+        coords=coords,
+    )
+    da.attrs = {
+        "variable_id": "t2",
+        "extended_description": "Temperature of the air 2m above Earth's surface.",
+        "units": "degC",
+        "data_type": "Gridded",
+        "resolution": "9 km",
+        "frequency": "hourly",
+        "location_subset": ["coordinate selection"],
+        "approach": "Time",
+        "downscaling_method": "Dynamical",
+        "institution": "UCLA",
+        "grid_mapping": "Lambert_Conformal",
+        "timezone": "America/Los_Angeles",
+    }
+    return da.to_dataset()
+
+
+def stand_in_grouped():
+    test_data = np.arange(0, 36, 1)
+    test_data = np.expand_dims(test_data, [1, 2])
+    coords = {
+        "x": 7.819e05,
+        "y": -4.116e06,
+        "lakemask": 0,
+        "landmask": 0,
+        "Lambert_Conformal": 0,
+        "time": pd.date_range(start="2001-01-01-00", end="2003-12-31-23", freq="1ME"),
+        "scenario": ["Historical + SSP 3-7.0"],
+        "simulation": ["WRF_EC-Earth3_r1i1p1f1"],
+    }
+    da = xr.DataArray(
+        name="Air Temperature at 2m",
+        dims=["time", "scenario", "simulation"],
+        data=test_data,
+        coords=coords,
+    )
+    return da.groupby("time.year").sum()
+
+
+def stand_in_daily():
+    test_data = np.arange(0, 365 * 3, 1)
+    test_data = test_data = np.expand_dims(test_data, [1, 2]) * np.ones((1, 1, 2))
+    coords = {
+        "x": 7.819e05,
+        "y": -4.116e06,
+        "lakemask": 0,
+        "landmask": 0,
+        "Lambert_Conformal": 0,
+        "time": pd.date_range(start="2001-01-01-00", end="2003-12-31-23", freq="1D"),
+        "scenario": ["Historical + SSP 3-7.0"],
+        "simulation": ["WRF_EC-Earth3_r1i1p1f1", "WRF_MPI-ESM1-2-HR_r3i1p1f1"],
+    }
+    da = xr.DataArray(
+        name="name",
+        dims=["time", "scenario", "simulation"],
+        data=test_data,
+        coords=coords,
+    )
+    da.attrs = {
+        "variable_id": "",
+        "extended_description": "",
+        "units": "",
+        "data_type": "Gridded",
+        "resolution": "9 km",
+        "frequency": "daily",
+        "location_subset": ["coordinate selection"],
+        "approach": "Time",
+        "downscaling_method": "Dynamical",
+        "institution": "UCLA",
+        "grid_mapping": "Lambert_Conformal",
+        "timezone": "America/Los_Angeles",
+    }
+    return da
 
 
 class TestTMYClass:
@@ -302,32 +414,55 @@ class TestTMYClass:
         assert result.lon.data == -117.829834
         assert (result.simulation.values == tmy.data_models).all()
 
-    @patch("climakitae.core.data_interface.get_data", return_value=mock_da_hourly)
-    def test__get_tmy_variable(self, mock_da_hourly):
-        """Check that daily stat gets returned."""
-        stn_name = "Santa Ana John Wayne Airport (KSNA)"
-        start_year = 2001
-        end_year = 2003
+    def test__get_tmy_variable(self, mock_t_hourly):
+        """Check that daily stat gets returned and values match expected."""
+        lat = 33.56
+        lon = -117.81
+        start_year = 1990
+        end_year = 2020
         # Initialize TMY object
-        tmy = TMY(start_year, end_year, station_name=stn_name)
-        result = tmy._get_tmy_variable("temperature", "K", ["max"])
-        assert isinstance(result, list)
-        assert len(result[0].time) == 365 * 3
+        tmy = TMY(start_year, end_year, latitude=lat, longitude=lon)
+        # Actually going to load data for a single point and check results
+        with patch.object(tmy, "_load_single_variable", return_value=mock_t_hourly):
+            result = tmy._get_tmy_variable(
+                "Air Temperature at 2m", "degC", ["max", "min", "mean", "sum"]
+            )
+            assert isinstance(result, list)
+            # Check attributes of first result
+            assert result[0].name == "Air Temperature at 2m"
+            assert result[0].attrs["units"] == "degC"
+            assert (result[0].simulation.values == mock_t_hourly.simulation).all()
+            assert result[0].attrs["frequency"] == "daily"
+            # Check all stats match
+            assert result[0].equals(mock_t_hourly.resample(time="1D").max())
+            assert result[1].equals(mock_t_hourly.resample(time="1D").min())
+            assert result[2].equals(mock_t_hourly.resample(time="1D").mean())
+            assert result[3].equals(mock_t_hourly.resample(time="1D").sum())
 
-    '''def test_load_all_variables(self):
+    def test_load_all_variables(self, mock_t_hourly):
         """Check that data load gets called and results merged."""
         stn_name = "Santa Ana John Wayne Airport (KSNA)"
         start_year = 2001
         end_year = 2003
         # Initialize TMY object
-        varlist = ["Daily max air temperature","Daily min air temperature","Daily mean air temperature", "Daily max dewpoint temperature","Daily min dewpoint temperature","Daily mean dewpoint temperature","Daily max wind speed","Daily mean wind speed","Global horizontal irradiance","Direct normal irradiance"]
+        varlist = [
+            "Daily max air temperature",
+            "Daily min air temperature",
+            "Daily mean air temperature",
+            "Daily max dewpoint temperature",
+            "Daily min dewpoint temperature",
+            "Daily mean dewpoint temperature",
+            "Daily max wind speed",
+            "Daily mean wind speed",
+            "Global horizontal irradiance",
+            "Direct normal irradiance",
+        ]
         tmy = TMY(start_year, end_year, station_name=stn_name)
-        with patch.object(tmy, "get_tmy_variable", return_value = [xr.DataArray(name="name",data=np.array([1])) for x in range(0,3)]):
-            result = tmy.load_all_variables()
-            assert isinstance(tmy.all_vars,xr.Dataset)
+        with patch.object(tmy, "_load_single_variable", return_value=mock_t_hourly):
+            tmy.load_all_variables()
+            assert isinstance(tmy.all_vars, xr.Dataset)
             for varname in varlist:
                 assert varname in tmy.all_vars
-        '''
 
     def test_set_cdf_climatology(self):
         """Check that data load and get_cdf get called."""
@@ -351,7 +486,7 @@ class TestTMYClass:
 
     @patch(
         "climakitae.explore.typical_meteorological_year.get_cdf_monthly",
-        return_value=mock_da_daily().groupby("time.year").sum(),
+        return_value=stand_in_grouped(),
     )
     def test_cdf_monthly(self, mock_get_cdf_monthly):
         """Check that data load and get_cdf_monthly get called."""
@@ -366,3 +501,76 @@ class TestTMYClass:
             mock_load.assert_called_once()
             mock_get_cdf_monthly.assert_called_once()
             assert tmy.cdf_monthly is not UNSET
+
+    def test_generate_tmy(self):
+        stn_name = "Santa Ana John Wayne Airport (KSNA)"
+        start_year = 2001
+        end_year = 2003
+        # Initialize TMY object
+        tmy = TMY(start_year, end_year, station_name=stn_name)
+        with (
+            patch.object(tmy, "load_all_variables") as mock_load,
+            patch.object(tmy, "get_candidate_months") as mock_get_months,
+            patch.object(tmy, "run_tmy_analysis") as mock_run_tmy,
+            patch.object(tmy, "export_tmy_data") as mock_export,
+        ):
+            tmy.generate_tmy()
+            mock_load.assert_called_once()
+            mock_get_months.assert_called_once()
+            mock_run_tmy.assert_called_once()
+            mock_export.assert_called_once()
+
+    def test_get_candidate_months(self):
+        stn_name = "Santa Ana John Wayne Airport (KSNA)"
+        start_year = 2001
+        end_year = 2003
+        # Initialize TMY object
+        tmy = TMY(start_year, end_year, station_name=stn_name)
+        with (
+            patch.object(tmy, "set_cdf_monthly") as mock_month,
+            patch.object(tmy, "set_cdf_climatology") as mock_clim,
+            patch.object(tmy, "set_weighted_statistic") as mock_weight,
+            patch.object(tmy, "calculate_top_df") as mock_top_df,
+        ):
+            tmy.get_candidate_months()
+            mock_clim.assert_called_once()
+            mock_month.assert_called_once()
+            mock_weight.assert_called_once()
+            mock_top_df.assert_called_once()
+
+    def test__make_tables(self):
+        data = {
+            "month": list(range(1, 13)),
+            "simulation": ["WRF_EC-Earth3_r1i1p1f1" for x in range(0, 12)],
+            "year": [2001 for x in range(0, 12)],
+        }
+        df = pd.DataFrame.from_dict(data)
+        all_vars_ds = mock_t_ds()
+
+        stn_name = "Santa Ana John Wayne Airport (KSNA)"
+        start_year = 2001
+        end_year = 2003
+        # Initialize TMY object
+        tmy = TMY(start_year, end_year, station_name=stn_name)
+        result = tmy._make_8760_tables(all_vars_ds, df)
+        # Check result dict of dataframes (only 1 for 1 simulation in test)
+        assert list(result.keys()) == list(all_vars_ds.simulation.values)
+        assert (
+            result["WRF_EC-Earth3_r1i1p1f1"].columns
+            == [
+                "time",
+                "scenario",
+                "simulation",
+                "x",
+                "y",
+                "lakemask",
+                "landmask",
+                "Lambert_Conformal",
+                "Air Temperature at 2m",
+            ]
+        ).all()
+        assert len(result["WRF_EC-Earth3_r1i1p1f1"].index) == 8760
+
+    def test_calculate_top_df(self):
+        # self.weighted_fs xarray.DataArray (simulation: 4, year: 27, month: 12)
+        pass
