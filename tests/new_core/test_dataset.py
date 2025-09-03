663e015d2c0a260991657c34628adf7a2a4400d2
Eric Lehmer <elehmer@berkeley.edu>
2025-09-03 13:48:41 -0700

    Add test_with_catalog_has_get_data

--- tests/new_core/test_dataset.py
+++ tests/new_core/test_dataset.py
@@ -58,3 +58,23 @@ class TestDatasetWithCatalogMethod:
             dataset.with_catalog(dictionary)
         except TypeError as e:
             assert "Data catalog must be an instance of DataCatalog." in str(e)
+
+    @patch("climakitae.new_core.dataset_factory.DataCatalog")
+    def test_with_catalog_has_get_data(self, mock_data_catalog):
+        """Test with_catalog input catalog has 'get_data' method."""
+        mock_catalog_instance = MagicMock()
+        mock_catalog_instance.catalog_df = pd.DataFrame(
+            {
+                "catalog": ["test_catalog", "another_catalog"],
+                "variable_id": ["var1", "var2"],
+            }
+        )
+        del mock_catalog_instance,get_data
+        mock_data_catalog.return_value = mock_catalog_instance
+
+        dataset = Dataset()
+
+        try:
+            dataset.with_catalog(mock_data_catalog)
+        except AttributeError as e:
+            assert "Data catalog must have a 'get_data' method to retrieve data." in str(e)
