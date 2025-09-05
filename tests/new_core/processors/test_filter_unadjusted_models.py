4e163c59823c82db465fd666cce6bfa58d48bd32
claalmve <chencalvin99@berkeley.edu>
2025-09-05 16:10:18 -0700

    Pushing more tests for execute, covering more data types for rest of tests

--- tests/new_core/processors/test_filter_unadjusted_models.py
+++ tests/new_core/processors/test_filter_unadjusted_models.py
@@ -13,7 +13,12 @@ import pandas as pd
 import pytest
 import xarray as xr
 
-from climakitae.core.constants import NON_WRF_BA_MODELS, UNSET, WRF_BA_MODELS
+from climakitae.core.constants import (
+    _NEW_ATTRS_KEY,
+    NON_WRF_BA_MODELS,
+    UNSET,
+    WRF_BA_MODELS,
+)
 from climakitae.new_core.processors.filter_unadjusted_models import (
     FilterUnAdjustedModels,
 )
@@ -41,14 +46,17 @@ class TestFilterUnAdjustedModelsInitialization:
 
     @pytest.mark.parametrize("invalid_value", ["y", "n", "maybe", "", "123"])
     def test_init_invalid_value(self, invalid_value):
-        """Test initialization with invalid values."""
+        """Test initialization and dummy execution with invalid values."""
         with pytest.raises(ValueError) as excinfo:
-            FilterUnAdjustedModels(value=invalid_value)
+            obj = FilterUnAdjustedModels(value=invalid_value)
+            result = xr.Dataset()
+            obj.execute(result, context={})
         assert "Invalid value" in str(excinfo.value)
+        assert "Valid values are:" in str(excinfo.value)
         assert any(valid in str(excinfo.value) for valid in ["yes", "no"])
 
 
-class TestFilterUnAdjustedModelsContainsUnadjustedModels:
+class TestFilterUnAdjustedModelsContainsUnAdjustedModels:
     """Test class for _contains_unadjusted_models method."""
 
     def setup_method(self):
@@ -230,7 +238,7 @@ class TestFilterUnAdjustedModelsContainsUnadjustedModels:
         assert "Unsupported type for result" in str(excinfo.value)
 
 
-class TestFilterUnAdjustedModelsRemoveUnadjustedModels:
+class TestFilterUnAdjustedModelsRemoveUnAdjustedModels:
     """Test class for _remove_unadjusted_models method."""
 
     def setup_method(self):
@@ -268,195 +276,45 @@ class TestFilterUnAdjustedModelsRemoveUnadjustedModels:
     def test_remove_unadjusted_models_single_dataset_adjusted(self):
         """Test _remove_unadjusted_models with a single adjusted Dataset."""
         result_yes = self.processor_yes._remove_unadjusted_models(self.ds_adjusted)
-        result_no = self.processor_no._remove_unadjusted_models(self.ds_adjusted)
         assert result_yes is self.ds_adjusted
-        assert result_no is self.ds_adjusted
 
     def test_remove_unadjusted_models_single_dataset_not_adjusted(self):
         """Test _remove_unadjusted_models with a single unadjusted Dataset."""
         result_yes = self.processor_yes._remove_unadjusted_models(self.ds_not_adjusted)
-        result_no = self.processor_no._remove_unadjusted_models(self.ds_not_adjusted)
         assert result_yes is None
-        assert result_no is self.ds_not_adjusted
 
     def test_remove_unadjusted_models_empty_dataset(self):
         """Test _remove_unadjusted_models with an empty Dataset."""
-        result_yes = self.processor_yes._remove_unadjusted_models(self.ds_empty)
-        result_no = self.processor_no._remove_unadjusted_models(self.ds_empty)
-        assert result_yes is self.ds_empty
-        assert result_no is self.ds_empty
+        result = self.processor_yes._remove_unadjusted_models(self.ds_empty)
+        assert result is self.ds_empty
 
     def test_remove_unadjusted_models_list_mixed(self):
         """Test _remove_unadjusted_models with a list of mixed Datasets."""
         result_yes = self.processor_yes._remove_unadjusted_models(
             [self.ds_adjusted, self.ds_not_adjusted]
         )
-        result_no = self.processor_no._remove_unadjusted_models(
-            [self.ds_adjusted, self.ds_not_adjusted]
-        )
         assert result_yes == [self.ds_adjusted]
-        assert result_no == [self.ds_adjusted, self.ds_not_adjusted]
 
     def test_remove_unadjusted_models_tuple_mixed(self):
         """Test _remove_unadjusted_models with a tuple of mixed Datasets."""
         result_yes = self.processor_yes._remove_unadjusted_models(
             (self.ds_not_adjusted, self.ds_adjusted)
         )
-        result_no = self.processor_no._remove_unadjusted_models(
-            (self.ds_not_adjusted, self.ds_adjusted)
-        )
-        assert result_yes == (self.ds_adjusted,)
-        assert result_no == (self.ds_not_adjusted, self.ds_adjusted)
+        assert result_yes == (self.ds_adjusted)
 
     def test_remove_unadjusted_models_list_all_not_adjusted(self):
         """Test _remove_unadjusted_models with a list of unadjusted Datasets."""
         result_yes = self.processor_yes._remove_unadjusted_models(
             [self.ds_not_adjusted, self.ds_not_adjusted]
         )
-        result_no = self.processor_no._remove_unadjusted_models(
-            [self.ds_not_adjusted, self.ds_not_adjusted]
-        )
-        assert result_yes is None
-        assert result_no == [self.ds_not_adjusted, self.ds_not_adjusted]
+        assert result_yes == []
 
     def test_remove_unadjusted_models_tuple_all_not_adjusted(self):
         """Test _remove_unadjusted_models with a tuple of unadjusted Datasets."""
         result_yes = self.processor_yes._remove_unadjusted_models(
             (self.ds_not_adjusted, self.ds_not_adjusted)
         )
-        result_no = self.processor_no._remove_unadjusted_models(
-            (self.ds_not_adjusted, self.ds_not_adjusted)
-        )
         assert result_yes is None
-        assert result_no == (self.ds_not_adjusted, self.ds_not_adjusted)
-
-    def test_remove_unadjusted_models_empty_list(self):
-        """Test _remove_unadjusted_models with an empty list."""
-        result_yes = self.processor_yes._remove_unadjusted_models([])
-        result_no = self.processor_no._remove_unadjusted_models([])
-        assert result_yes == []
-        assert result_no == []
-
-    def test_remove_unadjusted_models_empty_tuple(self):
-        """Test _remove_unadjusted_models with an empty tuple."""
-        result_yes = self.processor_yes._remove_unadjusted_models(())
-        result_no = self.processor_no._remove_unadjusted_models(())
-        assert result_yes == ()
-        assert result_no == ()
-
-    def test_remove_unadjusted_models_list_all_adjusted(self):
-        """Test _remove_unadjusted_models with a list of adjusted Datasets."""
-        result_yes = self.processor_yes._remove_unadjusted_models(
-            [self.ds_adjusted, self.ds_adjusted]
-        )
-        result_no = self.processor_no._remove_unadjusted_models(
-            [self.ds_adjusted, self.ds_adjusted]
-        )
-        assert result_yes == [self.ds_adjusted, self.ds_adjusted]
-        assert result_no == [self.ds_adjusted, self.ds_adjusted]
-
-    def test_remove_unadjusted_models_tuple_all_adjusted(self):
-        """Test _remove_unadjusted_models with a tuple of adjusted Datasets."""
-        result_yes = self.processor_yes._remove_unadjusted_models(
-            (self.ds_adjusted, self.ds_adjusted)
-        )
-        result_no = self.processor_no._remove_unadjusted_models(
-            (self.ds_adjusted, self.ds_adjusted)
-        )
-        assert result_yes == (self.ds_adjusted, self.ds_adjusted)
-        assert result_no == (self.ds_adjusted, self.ds_adjusted)
-
-    def test_remove_unadjusted_models_dict_mixed(self):
-        """Test _remove_unadjusted_models with a dictionary of mixed Datasets."""
-        result_yes = self.processor_yes._remove_unadjusted_models(
-            {"data1": self.ds_adjusted, "data2": self.ds_not_adjusted}
-        )
-        result_no = self.processor_no._remove_unadjusted_models(
-            {"data1": self.ds_adjusted, "data2": self.ds_not_adjusted}
-        )
-        assert result_yes == {"data1": self.ds_adjusted}
-        assert result_no == {"data1": self.ds_adjusted, "data2": self.ds_not_adjusted}
-
-    def test_remove_unadjusted_models_dict_all_not_adjusted(self):
-        """Test _remove_unadjusted_models with a dictionary of unadjusted Datasets."""
-        result_yes = self.processor_yes._remove_unadjusted_models(
-            {"data1": self.ds_not_adjusted, "data2": self.ds_not_adjusted}
-        )
-        result_no = self.processor_no._remove_unadjusted_models(
-            {"data1": self.ds_not_adjusted, "data2": self.ds_not_adjusted}
-        )
-        assert result_yes == {}
-        assert result_no == {
-            "data1": self.ds_not_adjusted,
-            "data2": self.ds_not_adjusted,
-        }
-
-    def test_remove_unadjusted_models_empty_dict(self):
-        """Test _remove_unadjusted_models with an empty dictionary."""
-        result_yes = self.processor_yes._remove_unadjusted_models({})
-        result_no = self.processor_no._remove_unadjusted_models({})
-        assert result_yes == {}
-        assert result_no == {}
-
-    def test_remove_unadjusted_models_dict_mixed_multiple(self):
-        """Test _remove_unadjusted_models with a dictionary with mixed Datasets."""
-        result_yes = self.processor_yes._remove_unadjusted_models(
-            {
-                "data1": self.ds_adjusted,
-                "data2": self.ds_adjusted,
-                "data3": self.ds_not_adjusted,
-            }
-        )
-        result_no = self.processor_no._remove_unadjusted_models(
-            {
-                "data1": self.ds_adjusted,
-                "data2": self.ds_adjusted,
-                "data3": self.ds_not_adjusted,
-            }
-        )
-        assert result_yes == {"data1": self.ds_adjusted, "data2": self.ds_adjusted}
-        assert result_no == {
-            "data1": self.ds_adjusted,
-            "data2": self.ds_adjusted,
-            "data3": self.ds_not_adjusted,
-        }
-
-    def test_remove_unadjusted_models_dict_all_adjusted(self):
-        """Test _remove_unadjusted_models with a dictionary of adjusted Datasets."""
-        result_yes = self.processor_yes._remove_unadjusted_models(
-            {
-                "data1": self.ds_adjusted,
-                "data2": self.ds_adjusted,
-                "data3": self.ds_adjusted,
-            }
-        )
-        result_no = self.processor_no._remove_unadjusted_models(
-            {
-                "data1": self.ds_adjusted,
-                "data2": self.ds_adjusted,
-                "data3": self.ds_adjusted,
-            }
-        )
-        assert result_yes == {
-            "data1": self.ds_adjusted,
-            "data2": self.ds_adjusted,
-            "data3": self.ds_adjusted,
-        }
-        assert result_no == {
-            "data1": self.ds_adjusted,
-            "data2": self.ds_adjusted,
-            "data3": self.ds_adjusted,
-        }
-
-    def test_remove_unadjusted_models_invalid_type(self):
-        """Test _remove_unadjusted_models with an invalid type."""
-        with pytest.raises(TypeError) as excinfo:
-            self.processor_yes._remove_unadjusted_models(123)
-        assert "Unsupported type for result" in str(excinfo.value)
-
-        with pytest.raises(TypeError) as excinfo:
-            self.processor_no._remove_unadjusted_models(123)
-        assert "Unsupported type for result" in str(excinfo.value)
 
 
 class TestFilterUnAdjustedModelsUpdateContext:
@@ -464,17 +322,128 @@ class TestFilterUnAdjustedModelsUpdateContext:
 
     def setup_method(self):
         """Setup method to initialize common variables."""
-        self.processor_yes = FilterUnAdjustedModels(value="yes")
-        self.processor_no = FilterUnAdjustedModels(value="no")
+        self.processor = FilterUnAdjustedModels()
 
     def test_update_context_yes(self):
         """Test update_context when value is 'yes'."""
         context = {}
-        self.processor_yes.update_context(context)
-        assert context.get("filter_unadjusted_models") is True
+        self.processor.update_context(context)
+        assert f"Process '{self.processor.name}' applied to the data" in context[
+            _NEW_ATTRS_KEY
+        ].get("filter_unadjusted_models")
 
-    def test_update_context_no(self):
-        """Test update_context when value is 'no'."""
-        context = {}
-        self.processor_no.update_context(context)
-        assert context.get("filter_unadjusted_models") is False
+    def test_update_context_with_attrs_key_existing(self):
+        """Test update_context when _NEW_ATTRS_KEY already exists in context."""
+        context = {_NEW_ATTRS_KEY: {"existing_key": "existing_value"}}
+        self.processor.update_context(context)
+        assert "existing_key" in context[_NEW_ATTRS_KEY]
+        assert f"Process '{self.processor.name}' applied to the data" in context[
+            _NEW_ATTRS_KEY
+        ].get("filter_unadjusted_models")
+
+
+class TestFilterUnAdjustedModelsExecute:
+    """Test class for execute method."""
+
+    def setup_method(self):
+        """Setup method to initialize common variables."""
+        self.processor_yes = FilterUnAdjustedModels(value="yes")
+        self.processor_no = FilterUnAdjustedModels(value="no")
+
+        # Create sample xarray Datasets and DataArrays for testing
+        self.ds_empty = xr.Dataset()
+        self.ds_adjusted = xr.Dataset(
+            {"temp": (["time"], [1, 2, 3])},
+            coords={
+                "time": pd.date_range("2000-01-01", periods=3),
+                "simulation": ["model_a"],
+            },
+            attrs={
+                "intake_esm_attrs:activity_id": "WRF",
+                "intake_esm_attrs:source_id": "EC-Earth3",
+                "intake_esm_attrs:member_id": "r1i1p1f1",
+            },
+        )
+        self.ds_not_adjusted = xr.Dataset(
+            {"temp": (["time"], [4, 5, 6])},
+            coords={
+                "time": pd.date_range("2000-01-01", periods=3),
+                "simulation": ["model_b"],
+            },
+            attrs={
+                "intake_esm_attrs:activity_id": "WRF",
+                "intake_esm_attrs:source_id": "FGOALS-g3",
+                "intake_esm_attrs:member_id": "r1i1p1f1",
+            },
+        )
+
+    def test_execute_single_dataset_adjusted_yes(self):
+        """Test execute with a single adjusted Dataset and value 'yes'."""
+        result = self.processor_yes.execute(self.ds_adjusted, context={})
+        assert result is self.ds_adjusted
+
+    def test_execute_single_dataset_not_adjusted_yes(self):
+        """Test execute with a single unadjusted Dataset and value 'yes'."""
+        result = self.processor_yes.execute(self.ds_not_adjusted, context={})
+        assert result is None
+
+    def test_execute_single_dataset_adjusted_no(self):
+        """Test execute with a single adjusted Dataset and value 'no'."""
+        result = self.processor_no.execute(self.ds_adjusted, context={})
+        assert result is self.ds_adjusted
+
+    def test_execute_single_dataset_not_adjusted_no(self):
+        """Test execute with a single unadjusted Dataset and value 'no'."""
+        result = self.processor_no.execute(self.ds_not_adjusted, context={})
+        assert result is self.ds_not_adjusted
+
+    def test_execute_list_mixed_yes(self):
+        """Test execute with a list of mixed Datasets and value 'yes'."""
+        result = self.processor_yes.execute(
+            [self.ds_adjusted, self.ds_not_adjusted], context={}
+        )
+        assert result == [self.ds_adjusted]
+
+    def test_execute_list_mixed_no(self):
+        """Test execute with a list of mixed Datasets and value 'no'."""
+        result = self.processor_no.execute(
+            [self.ds_adjusted, self.ds_not_adjusted], context={}
+        )
+        assert result == [self.ds_adjusted, self.ds_not_adjusted]
+
+    def test_execute_tuple_mixed_yes(self):
+        """Test execute with a tuple of mixed Datasets and value 'yes'."""
+        result = self.processor_yes.execute(
+            (self.ds_not_adjusted, self.ds_adjusted), context={}
+        )
+        assert result == (self.ds_adjusted,)
+
+    def test_execute_tuple_mixed_no(self):
+        """Test execute with a tuple of mixed Datasets and value 'no'."""
+        result = self.processor_no.execute(
+            (self.ds_not_adjusted, self.ds_adjusted), context={}
+        )
+        assert result == (self.ds_not_adjusted, self.ds_adjusted)
+
+    def test_execute_empty_list_yes(self):
+        """Test execute with an empty list and value 'yes'."""
+        result = self.processor_yes.execute([], context={})
+        assert result == []
+
+    def test_execute_empty_list_no(self):
+        """Test execute with an empty list and value 'no'."""
+        result = self.processor_no.execute([], context={})
+        assert result == []
+
+    def test_execute_throws_warning_case_yes(self):
+        """Test execute raises warning when all models are unadjusted and value 'yes'."""
+        with pytest.warns(UserWarning) as record:
+            result = self.processor_yes.execute(
+                [self.ds_not_adjusted, self.ds_not_adjusted], context={}
+            )
+            assert result == []
+        assert any(
+            "Your query selected models that do not have a-priori bias adjustment."
+            in str(warning.message)
+            for warning in record
+        )
