230ae3ddc9a27f247524f583c793cc82c0363acf
neilSchroeder <neil.raymond.schroeder@gmail.clom>
2025-10-07 10:05:27 -0500

    pulling in clip from new-core

--- climakitae/new_core/param_validation/__init__.py
+++ climakitae/new_core/param_validation/__init__.py
@@ -1,6 +1,7 @@
 """Import param validation classes ensuring they are registered."""
 
 from .concat_param_validator import validate_concat_param
+from .clip_param_validator import validate_clip_param
 from .data_param_validator import DataValidator
 from .filter_unadjusted_models_param_validator import (
     validate_filter_unadjusted_models_param,
--- climakitae/new_core/processors/__init__.py
+++ climakitae/new_core/processors/__init__.py
@@ -1,6 +1,7 @@
 """Initialize the processors, ensuring they get registered."""
 
 from .concatenate import Concat
+from .clip import Clip
 from .filter_unadjusted_models import FilterUnAdjustedModels
 from .update_attributes import UpdateAttributes
 
