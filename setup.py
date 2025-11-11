3018ca1406084ed1475bfa77772cc076c65ff5aa
neilSchroeder <neil.raymond.schroeder@gmail.clom>
2025-11-11 09:03:48 -0600

    setup file setup

--- setup.py
+++ setup.py
@@ -1,15 +1,4 @@
 #!/usr/bin/env python
-# read the contents of your README file
-from pathlib import Path
-
 from setuptools import setup
 
-this_directory = Path(__file__).parent
-long_description = (this_directory / "README.md").read_text()
-
-setup(
-    name="climakitae",
-    # other arguments omitted
-    long_description=long_description,
-    long_description_content_type="text/markdown",
-)
+setup()
