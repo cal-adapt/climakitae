6c5f1f3a54e39f771753218cb14f74f720170990
neilSchroeder <neil.raymond.schroeder@gmail.clom>
2025-11-11 09:28:20 -0600

    updating repo

--- climakitae/__init__.py
+++ climakitae/__init__.py
@@ -1,24 +1,15 @@
 from climakitae.core.data_export import export, remove_zarr
 from climakitae.core.data_load import load
-
-try:
-    from importlib.metadata import version as _version
-except ImportError:
-    # if the fallback library is missing, we are doomed.
-    from importlib_metadata import version as _version  # type: ignore[no-redef]
-
-try:
-    __version__ = _version("climakitae")
-except Exception:
-    # Local copy or not installed with setuptools.
-    # Disable minimum version checks on downstream libraries.
-    __version__ = "999"
+from climakitae._version import __version__
+from climakitae.new_core.user_interface import ClimateData
 
 __all__ = (
     # Methods
     "load",
     "export",
     "remove_zarr",
+    # Classes
+    "ClimateData",
     # Constants
     "__version__",
 )
