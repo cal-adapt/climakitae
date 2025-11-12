8c672cb4224799eb6b63632047d25ae8211d10a2
neilSchroeder <neil.raymond.schroeder@gmail.clom>
2025-11-12 09:05:39 -0600

    filtering external package warnings

--- climakitae/__init__.py
+++ climakitae/__init__.py
@@ -1,5 +1,10 @@
 import warnings
 
+from climakitae.core.data_export import export, remove_zarr
+from climakitae.core.data_load import load
+from climakitae._version import __version__
+from climakitae.new_core.user_interface import ClimateData
+
 # By default, ignore warnings coming from modules outside this package so that
 # noisy third-party warnings (for example pandas/dateutil parsing fallbacks)
 # don't appear for users who set logging to WARNING. Keep warnings that
@@ -8,13 +13,8 @@ import warnings
 #
 # We use a negative lookahead to match module names that do NOT start with
 # 'climakitae' and ignore those warnings; modules beginning with
-# 'climakitae' will not be ignored by this rule.
-warnings.filterwarnings("ignore", module=r"^(?!climakitae)")
-
-from climakitae.core.data_export import export, remove_zarr
-from climakitae.core.data_load import load
-from climakitae._version import __version__
-from climakitae.new_core.user_interface import ClimateData
+# 'climakitae' will not be ignored by this rule. Match full module names.
+warnings.filterwarnings("ignore", module=r"^(?!climakitae).*")
 
 __all__ = (
     # Methods
