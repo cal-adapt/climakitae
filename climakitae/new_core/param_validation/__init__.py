e6c8ee9eda1a5f04c2c2630949c4416ac6c29914
claalmve <chencalvin99@berkeley.edu>
2025-11-25 10:08:07 +0530

    Adding init functionality to processors and param validators

--- climakitae/new_core/param_validation/__init__.py
+++ climakitae/new_core/param_validation/__init__.py
@@ -1,16 +1,17 @@
 """Import param validation classes ensuring they are registered."""
 
+from .bias_adjust_model_to_station_param_validator import (
+    validate_bias_correction_station_data_param,
+)
+from .clip_param_validator import validate_clip_param
 from .concat_param_validator import validate_concat_param
 from .convert_units_param_validator import validate_convert_units_param
-from .clip_param_validator import validate_clip_param
 from .data_param_validator import DataValidator
 from .filter_unadjusted_models_param_validator import (
     validate_filter_unadjusted_models_param,
 )
+from .metric_calc_param_validator import validate_metric_calc_param
 from .renewables_param_validator import RenewablesValidator
 from .time_slice_param_validator import validate_time_slice_param
 from .update_attributes_param_validator import validate_update_attributes_param
 from .warming_param_validator import validate_warming_level_param
-from .bias_adjust_model_to_station_param_validator import (
-    validate_bias_correction_station_data_param,
-)
