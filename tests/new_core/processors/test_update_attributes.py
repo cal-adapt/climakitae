ddd3d76b8191cadeaaf96d2b1074ccaa01bf9446
neilSchroeder <neil.raymond.schroeder@gmail.clom>
2025-12-16 09:13:36 -0600

    Add test_update_context_creates_new_attrs_key

--- tests/new_core/processors/test_update_attributes.py
+++ tests/new_core/processors/test_update_attributes.py
@@ -29,3 +29,17 @@ class TestUpdateAttributesInit:
         processor = UpdateAttributes(value="custom_value")
         assert processor.value == "custom_value"
         assert processor.name == "update_attributes"
+
+
+class TestUpdateAttributesUpdateContext:
+    """Test class for UpdateAttributes update_context method."""
+
+    def test_update_context_creates_new_attrs_key(self):
+        """Test that update_context creates _NEW_ATTRS_KEY if not present."""
+        processor = UpdateAttributes()
+        context = {}
+
+        processor.update_context(context)
+
+        assert _NEW_ATTRS_KEY in context
+        assert isinstance(context[_NEW_ATTRS_KEY], dict)
