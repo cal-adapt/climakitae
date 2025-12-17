0da7e8f70855da9db195548261e48200e86f50a4
claalmve <chencalvin99@berkeley.edu>
2025-12-17 14:13:18 -0800

    Remove debugger

--- climakitae/new_core/processors/metric_calc.py
+++ climakitae/new_core/processors/metric_calc.py
@@ -766,9 +766,6 @@ class MetricCalc(DataProcessor):
                         vectorize=True,  # auto-loop over lat/lon or y/x or spatial_1/spatial_2
                         dask="parallelized",  # works with lazy dask arrays
                     )
-                    import pdb
-
-                    pdb.set_trace()
                     return_values = return_values.assign_coords(
                         return_period=self.return_periods
                     )
