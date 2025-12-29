764058e349c7755ee2428674680fb54e81b72410
neilSchroeder <neil.raymond.schroeder@gmail.com>
2025-12-29 09:20:38 -0600

    Refactor __init__.py to include derived variable functions in __all__ export

--- climakitae/new_core/__init__.py
+++ climakitae/new_core/__init__.py
@@ -1,4 +1,19 @@
 from .data_access.data_access import DataCatalog
+from .derived_variables import (
+    get_registry,
+    list_derived_variables,
+    register_derived,
+    register_user_function,
+)
 
 CATALOG = DataCatalog()
 CATALOG.list_clip_boundaries()
+
+__all__ = [
+    "DataCatalog",
+    "CATALOG",
+    "get_registry",
+    "register_derived",
+    "register_user_function",
+    "list_derived_variables",
+]
