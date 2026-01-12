c81dff31a4dfab52e848fb111e92777720e6c33a
name <vanessa.machuca@eaglerockanalytics.com>
2026-01-12 15:16:42 -0800

    first pass at addig warming_level_window to profile retrieval and export naming functions complete

--- climakitae/explore/standard_year_profile.py
+++ climakitae/explore/standard_year_profile.py
@@ -115,7 +115,12 @@ def _convert_stations_to_lat_lon(
 
 
 def _get_clean_standardyr_filename(
-    var_id: str, q: float, location: str, gwl: float, no_delta: bool
+    var_id: str,
+    q: float,
+    location: str,
+    gwl: float,
+    warming_level_window: int | None,
+    no_delta: bool,
 ) -> str:
     """
     Standardizes filename export for standard year files
@@ -130,6 +135,9 @@ def _get_clean_standardyr_filename(
         String describing profile location
     gwl : float
         Single gwl for csv file name
+    warming_level_window: int
+        Years around Global Warming Level (+/-) (e.g. 15 means a 30yr window)
+
     no_delta : bool
         no_delta value used to generate profile
 
@@ -149,7 +157,12 @@ def _get_clean_standardyr_filename(
     else:
         delta_str = "_delta_from_historical"
 
-    filename = f"stdyr_{clean_var_name}_{clean_q_name}ptile_{clean_loc_name}_{clean_gwl_name}{delta_str}.csv"
+    if warming_level_window is None:
+        window_str = ""
+    else:
+        window_str = "_{warming_level_window}yr_window"
+
+    filename = f"stdyr_{clean_var_name}_{clean_q_name}ptile_{clean_loc_name}_{clean_gwl_name}{delta_str}{window_str}.csv"
     return filename
 
 
@@ -266,6 +279,8 @@ def export_profile_to_csv(profile, **kwargs):
                 Percentile used in profile
             global_warming_levels : list[float]
                 List of global warming levels in profile
+            warming_level_winow: int in range (5,25), optional
+                Years around Global Warming Level (+/-) (e.g. 15 means a 30yr window)
             latitude : tuple(float | int), optional
                 Latitude coordinate range from profile location
             longitude : tuple(float | int), optional
@@ -298,8 +313,9 @@ def export_profile_to_csv(profile, **kwargs):
     q = kwargs.get("q")
     global_warming_levels = kwargs.get("warming_level")
 
-    # Handle no_delta input
+    # Handle no_delta wand warming_level_window inputs
     no_delta = kwargs.get("no_delta", False)
+    warming_level_window = kwargs.get("warming_level_window", None)
 
     # Get variable id string to use in file name
     variable_descriptions = read_csv_file(VARIABLE_DESCRIPTIONS_CSV_PATH)
@@ -319,13 +335,13 @@ def export_profile_to_csv(profile, **kwargs):
         case 2:  # Single WL
             gwl = global_warming_levels[0]
             filename = _get_clean_standardyr_filename(
-                var_id, q, location_str, gwl, no_delta
+                var_id, q, location_str, gwl, warming_level_window, no_delta
             )
             profile.to_csv(filename)
         case 3:  # Multiple WL (WL included in MultiIndex)
             for gwl in global_warming_levels:  # Single file per WL
                 filename = _get_clean_standardyr_filename(
-                    var_id, q, location_str, gwl, no_delta
+                    var_id, q, location_str, gwl, warming_level_window, no_delta
                 )
                 profile.xs(f"WL_{gwl}", level="Warming_Level", axis=1).to_csv(filename)
         case _:
@@ -347,11 +363,11 @@ def retrieve_profile_data(**kwargs: any) -> Tuple[xr.Dataset, xr.Dataset]:
         - variable (Optional) : str, default "Air Temperature at 2m"
         - resolution (Optional) : str, default "3 km"
         - warming_levels (Optional) : List[float], default [1.2]
+        - warming_level_window (Optional): int in range (5,25)
         - cached_area (Optional) : str or List[str]
         - latitude (Optional) : float or tuple
         - longitude (Optional) : float or tuple
         - stations (Optional) : list[str], default None
-        - warming_level_window (Optional): int in range (5,25)
         - units (Optional) : str, default "degF"
         - no_delta (optional) : bool, default False, if True, do not retrieve historical data, return raw future profile
 
@@ -522,7 +538,7 @@ def retrieve_profile_data(**kwargs: any) -> Tuple[xr.Dataset, xr.Dataset]:
         ),
         "approach": "Warming Level",
         "warming_level": [1.2],
-        "warming_level_window": kwargs.get('warming_level_window', None),
+        "warming_level_window": kwargs.get("warming_level_window", None),
         "cached_area": kwargs.get("cached_area", None),
         "latitude": kwargs.get("latitude", None),
         "longitude": kwargs.get("longitude", None),
@@ -555,12 +571,12 @@ def get_climate_profile(**kwargs) -> pd.DataFrame:
         - variable (Optional) : str, default "Air Temperature at 2m"
         - resolution (Optional) : str, default "3 km"
         - warming_level (Required) : List[float], default [1.2]
+        - warming_level_window (Optional): int in range (5,25)
         - cached_area (Optional) : str or List[str]
         - units (Optional) : str, default "degF"
         - latitude (Optional) : float or tuple
         - longitude (Optional) : float or tuple
         - stations (Optional) : list[str], default None
-        - warming_level_window (Optional): int in range (5,25)
         - days_in_year (Optional) : int, default 365
         - q (Optional) : float | list[float], default 0.5, quantile for profile calculation
         - no_delta (optional) : bool, default False, if True, do not apply baseline subtraction, return raw future profile
@@ -587,7 +603,6 @@ def get_climate_profile(**kwargs) -> pd.DataFrame:
     days_in_year = kwargs.pop("days_in_year", 365)
     q = kwargs.pop("q", 0.5)
     no_delta = kwargs.get("no_delta", False)
-    warming_level_window = kwargs.get('warming_level_window', False)
 
     # Retrieve the climate data
     print("📊 Retrieving climate data...")
