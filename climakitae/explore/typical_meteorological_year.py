5335c1d9f54c6b4825e9f355dffa419ae57b26d2
Victoria Ford <fordvecellio@gmail.com>
2026-06-25 13:09:37 -0500

    add floating point precision for TMY

--- climakitae/explore/typical_meteorological_year.py
+++ climakitae/explore/typical_meteorological_year.py
@@ -1279,6 +1279,26 @@ class TMY:
             else:
                 tmy_data_to_export[sim]["scenario"] = "historical+ssp370"
 
+            # Set floating point precision to 1 decimal for all vars
+            # except wind speed which is 2 decimals
+            float_cols = [
+                "Air Temperature at 2m", 
+                "Dew point temperature", 
+                "Relative humidity",
+                "Surface Pressure",
+                "Instantaneous downwelling longwave flux at bottom",
+                "Instantaneous downwelling shortwave flux at bottom",
+                "Shortwave surface downward direct normal irradiance",
+                "Shortwave surface downward diffuse irradiance",
+                "Wind direction at 10m",
+                ]
+            tmy_data_to_export[sim][float_cols] = (
+                tmy_data_to_export[sim][float_cols].round(1)
+            )
+            tmy_data_to_export[sim]["Wind speed at 10m"] = (
+                tmy_data_to_export[sim]["Wind speed at 10m"].round(2)
+            )
+
         self.tmy_data_to_export = tmy_data_to_export
         self._vprint("TMY analysis complete.")
 
